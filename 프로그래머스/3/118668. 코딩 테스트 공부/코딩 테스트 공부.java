862d8b763ccdadf433c1fb4a8fe37ce6cf245be0
biddan606 <153509440+biddan606@users.noreply.github.com>
2023-12-17 19:47:58 +0900

    [level 3] Title: 코딩 테스트 공부, Time: 21.37 ms, Memory: 52.6 MB -BaekjoonHub

--- 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.java
+++ 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.java
@@ -1,3 +1,10 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Queue;
+import java.util.stream.Collectors;
+
 class Solution {
 
     public int solution(int alp, int cop, int[][] problems) {
@@ -9,38 +16,42 @@ class Solution {
             maxCol = Math.max(maxCol, problem[1]);
         }
 
-        // 공부를 통한 능력 올리기
+        // 코스트 초기화
         int[][] costs = new int[maxRow - alp + 1][maxCol - cop + 1];
         for (int row = 0; row < costs.length; row++) {
             for (int col = 0; col < costs[0].length; col++) {
-                costs[row][col] = row + col;
+                costs[row][col] = Integer.MAX_VALUE;
             }
         }
+        costs[0][0] = 0;
 
-        // DP
-        for (int row = 0; row < costs.length; row++) {
-            for (int col = 0; col < costs[0].length; col++) {
-                if (row + 1 < costs.length) {
-                    costs[row + 1][col] = Math.min(costs[row][col] + 1, costs[row + 1][col]);
-                }
-                if (col + 1 < costs[0].length) {
-                    costs[row][col + 1] = Math.min(costs[row][col] + 1, costs[row][col + 1]);
-                }
+        List<int[]> problemsWithAddedStudy = new ArrayList<>(Arrays.stream(problems)
+                .map(a -> new int[]{a[0] - alp, a[1] - cop, a[2], a[3], a[4]})
+                .collect(Collectors.toList()));
+        problemsWithAddedStudy.add(new int[]{0, 0, 1, 0, 1});
+        problemsWithAddedStudy.add(new int[]{0, 0, 0, 1, 1});
 
-                for (int[] problem : problems) {
-                    if (row < Math.max(0, problem[0] - alp) || col < Math.max(0, problem[1] - cop)) {
-                        continue;
-                    }
+        Queue<int[]> queue = new ArrayDeque<>();
+        queue.offer(new int[]{0, 0, 0});
 
-                    int problemRow = Math.min(costs.length - 1, row + problem[2]);
-                    int problemCol = Math.min(costs[0].length - 1, col + problem[3]);
+        while (!queue.isEmpty()) {
+            int[] currentPointInformation = queue.poll();
 
-                    costs[problemRow][ problemCol] = Math.min(
-                            costs[problemRow][problemCol],
-                            costs[row][col] + problem[4]
-                    );
-                }
-            }
+            problemsWithAddedStudy.stream()
+                    .filter(problem -> currentPointInformation[0] >= problem[0]
+                            && currentPointInformation[1] >= problem[1])
+                    .forEach(problem -> {
+                        int nextRow = Math.min(currentPointInformation[0] + problem[2], costs.length - 1);
+                        int nextCol = Math.min(currentPointInformation[1] + problem[3], costs[0].length - 1);
+                        int nextCost = currentPointInformation[2] + problem[4];
+
+                        if (nextCost >= costs[nextRow][nextCol]) {
+                            return;
+                        }
+                        costs[nextRow][nextCol] = nextCost;
+
+                        queue.offer(new int[]{nextRow, nextCol, nextCost});
+                    });
         }
 
         return costs[costs.length - 1][costs[0].length - 1];
