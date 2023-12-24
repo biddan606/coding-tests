09d1cfb3e383c6b317f0ca8f1d422915eb8712de
biddan606 <153509440+biddan606@users.noreply.github.com>
2023-12-24 22:04:47 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.09 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -1,34 +1,6 @@
 class Solution {
-
-    public String solution(String source) {
-        StringBuilder answer = new StringBuilder();
-
-        int index = 0;
-        for (char c : source.toCharArray()) {
-            if (Character.isSpaceChar(c)) {
-                answer.append(c);
-                index = 0;
-                continue;
-            }
-
-            char charToAdd = Character.toLowerCase(c);
-            if (index % 2 == 0) {
-                charToAdd = Character.toUpperCase(c);
-            }
-
-            answer.append(charToAdd);
-
-            index++;
-        }
-
-        return answer.toString();
-    }
-
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-
-        String result = solution.solution("try hello world");
-
-        System.out.println(result);
+    public String solution(String s) {
+        String answer = "";
+        return answer;
     }
 }
