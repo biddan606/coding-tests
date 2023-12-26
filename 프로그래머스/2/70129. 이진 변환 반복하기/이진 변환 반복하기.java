c79bc777981004b35dabd862f13071089128e4a2
biddan606 <153509440+biddan606@users.noreply.github.com>
2023-12-27 03:47:30 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 4.32 ms, Memory: 82.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -0,0 +1,52 @@
+import java.util.Arrays;
+
+class Solution {
+
+    public int[] solution(String str) {
+        int loopCount = 0;
+        int removedZeroCount = 0;
+
+        while (str.length() != 1) {
+            String newStr = removeZero(str);
+            removedZeroCount += str.length() - newStr.length();
+
+            str = Integer.toString(newStr.length(), 2);
+
+            loopCount++;
+        }
+
+        return new int[]{loopCount, removedZeroCount};
+    }
+
+    private int countOne(String str) {
+        int count = 0;
+
+        for (char c : str.toCharArray()) {
+            if (c == '1') {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    private String removeZero(String str) {
+        StringBuilder builder = new StringBuilder();
+
+        for (char c : str.toCharArray()) {
+            if (c == '0') {
+                continue;
+            }
+
+            builder.append(c);
+        }
+
+        return builder.toString();
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        System.out.println(Arrays.toString(solution.solution("110010101001")));
+    }
+}
