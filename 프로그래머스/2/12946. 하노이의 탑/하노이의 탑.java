d0dfa4c2e2dcd1cea758d0afcd11eeb3ba51fe90
biddan606 <153509440+biddan606@users.noreply.github.com>
2023-12-28 17:36:42 +0900

    [level 2] Title: 하노이의 탑, Time: 16.86 ms, Memory: 116 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
@@ -1,6 +1,41 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
     public int[][] solution(int n) {
-        int[][] answer = {};
-        return answer;
+        List<int[]> result = hanoi(n, 1, 3);
+
+        return result.toArray(int[][]::new);
+    }
+
+    private List<int[]> hanoi(int size, int from, int to) {
+        ArrayList<int[]> result = new ArrayList<>();
+
+        if (size == 1) {
+            result.add(new int[]{from, to});
+
+            return result;
+        }
+
+        int empty = 6 - from - to;
+
+        result.addAll(hanoi(size - 1, from, empty));
+        result.addAll(hanoi(1, from, to));
+        result.addAll(hanoi(size - 1, empty, to));
+
+        return result;
     }
 }
+
+/*
+(3, 1, 3) // 1, 2, 3
+    (2, 1, 2) // 1, 2 // 1, 2로 되어 있는 원판을 뒤집음
+        (1, 1, 3) // 1
+        (1, 1, 2) // 2
+        (1, 3, 2) // 1
+    (1, 1, 3) // 3 // 맨 밑에 있는 원판을 원하는 to로 이동
+    (2, 2, 3) // 2, 1 // 뒤집어진 원판들을 to로 이동
+        (1, 2, 1) // 1
+        (1, 2, 3) // 2
+        (1, 1, 3) // 1
+*/
