a17727451ed65828846b68d7f270a8db2403081c
biddan606 <153509440+biddan606@users.noreply.github.com>
2024-01-01 21:08:27 +0900

    [level 2] Title: 카펫, Time: 0.20 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,18 +1,16 @@
 class Solution {
-    public int[] solution(int brown, int yellow) {
-        int resultHeight = -1;
-        int resultWidth = -1;
-        
-        for (int height = 0; height < 2500; height++) {
+    public int[] solution(int brown, int yellow) {    
+        for (int height = 3; height < 2500; height++) {
             for (int width = height; width < 2500; width++) {
-                if (2 * (height + width) - 4 == brown && 
-                   (height - 2) * (width - 2) == yellow) {
-                    resultHeight = height;
-                    resultWidth = width;
+                int currentBrown = 2 * (height + width) - 4;
+                int currentYellow = (height - 2) * (width - 2);
+                
+                if (currentBrown == brown && currentYellow == yellow) {
+                    return new int[]{width, height};
                 }
             }
         }
         
-        return new int[]{resultWidth, resultHeight};
+        return null;
     }
 }
