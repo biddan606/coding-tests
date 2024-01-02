8dbc8bc0279c2a1e8aa3a7786e1cfe86501c3378
biddan606 <153509440+biddan606@users.noreply.github.com>
2024-01-03 00:45:42 +0900

    [level 3] Title: 불량 사용자, Time: 4.36 ms, Memory: 84 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
@@ -1,50 +1,6 @@
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
-
 class Solution {
-
-    public int solution(String[] userIds, String[] bannedIds) {
-        String[][] bannedGroup = Arrays.stream(bannedIds)
-                .map(bannedId -> bannedId.replace("*", "."))
-                .map(bannedId ->
-                        Arrays.stream(userIds)
-                                .filter(userId -> userId.matches(bannedId))
-                                .toArray(String[]::new)
-                ).toArray(String[][]::new);
-
-        Set<Set<String>> matchedResult = new HashSet<>();
-
-        countCases(bannedGroup, 0, new HashSet<>(), matchedResult);
-
-        return matchedResult.size();
-    }
-
-    private void countCases(String[][] bannedGroup, int bannedGroupIndex, Set<String> banned,
-            Set<Set<String>> matchedResult) {
-        if (bannedGroup.length == bannedGroupIndex) {
-            matchedResult.add(new HashSet<>(banned));
-            return;
-        }
-
-        for (String banningId : bannedGroup[bannedGroupIndex]) {
-            if (banned.contains(banningId)) {
-                continue;
-            }
-
-            banned.add(banningId);
-
-            countCases(bannedGroup, bannedGroupIndex + 1, banned, matchedResult);
-
-            banned.remove(banningId);
-        }
-    }
-
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        String[] userIds = {"frodo", "fradi", "crodo", "abc123", "frodoc"};
-        String[] bannedIds = {"fr*d*", "abc1**"};
-
-        System.out.println(solution.solution(userIds, bannedIds));
+    public int solution(String[] user_id, String[] banned_id) {
+        int answer = 0;
+        return answer;
     }
 }
