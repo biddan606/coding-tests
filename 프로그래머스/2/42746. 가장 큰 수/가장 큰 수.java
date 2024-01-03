9a0aa3e4172ac25df136743c0685a53835ea7dd1
biddan606 <153509440+biddan606@users.noreply.github.com>
2024-01-03 19:55:53 +0900

    [level 2] Title: 가장 큰 수, Time: 4.90 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+class Solution {
+
+    public String solution(int[] numbers) {
+        if (Arrays.stream(numbers).allMatch(i -> i == 0)) {
+            return "0";
+        }
+
+        return Arrays.stream(numbers)
+                .boxed()
+                .map(String::valueOf)
+                .sorted((s1, s2) -> (s2 + s1).compareTo(s1 + s2))
+                .collect(Collectors.joining());
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] numbers = {
+                0, 0, 0
+        };
+
+        String result = solution.solution(numbers);
+
+        System.out.println(result);
+    }
+}
