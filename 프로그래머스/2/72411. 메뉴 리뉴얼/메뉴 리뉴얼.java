12674f8de03f968d49ca26a29f6bf6d344c02b66
biddan606 <153509440+biddan606@users.noreply.github.com>
2024-01-04 15:25:24 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 37.18 ms, Memory: 94.6 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
@@ -1,92 +1,103 @@
-import java.util.*;
-import java.util.stream.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.TreeSet;
 
 class Solution {
+
     public String[] solution(String[] orders, int[] course) {
-        // 주문의 메뉴를 찾기 쉬운 형태인 Set<String>으로 변환한다.
-        List<Set<String>> orderList = Arrays.stream(orders)
-                .map(str -> Arrays.stream(str.split(""))
-                                .collect(Collectors.toSet()))
-                        .collect(Collectors.toList());
-
-        // 입력 받은 course 길이의 메뉴 후보만 받도록 초기 설정을 한다.
-        Map<Integer, List<Course>> courses = new HashMap<>();
-        for (int length : course) {
-            List<Course> list = new ArrayList<>();
-
-            list.add(new Course("", 0));
-
-            courses.put(length, list);
+        // orders를 정렬된 set으로 변환한다.
+        Set<String>[] orderSet = new Set[orders.length];
+        for (int i = 0; i < orderSet.length; i++) {
+            orderSet[i] = new TreeSet<>(List.of(orders[i].split("")));
         }
-        // 메뉴 후보를 받는다.
-        // course 길이 중 가장 많이 주문된 메뉴만 받는다.(중복 가능)
-        getCourses('A', new HashSet<>(), orderList, courses);
 
-        // 2번 이상 주문한 메뉴 조합만을 반환한다.
-        return courses.values().stream()
-                .filter(list -> list.get(0).occurrences >= 2)
-                .flatMap(List::stream)
-                .map(c -> c.name)
+        Set<String> menuCandidates = new HashSet<>();
+
+        // course 크기의 orders로 만든 코스요리를 만든다.
+        for (int size : course) {
+            List<Set<String>> courseMenus = new ArrayList<>();
+            generateCourseMenus(orderSet, size, courseMenus);
+
+            // 만든 코스요리가 몇번 주문 되었는지 계산한다.
+            Map<Integer, List<String>> courseMap = new HashMap<>();
+            for (Set<String> courseMenu : courseMenus) {
+                CourseCount courseCount = getCourseCount(orderSet, courseMenu);
+                courseMap.computeIfAbsent(courseCount.count, k -> new ArrayList<>())
+                        .add(courseCount.name);
+            }
+
+            // 해당 course count에서 가장 많이 주문된 요리를 메뉴 후보에 추가한다. (최소 2 이상이어야 한다)
+            List<String> addingCourses = courseMap.entrySet().stream()
+                    .filter(e -> e.getKey() >= 2)
+                    .max(Entry.comparingByKey())
+                    .map(Entry::getValue)
+                    .orElse(new ArrayList<>());
+
+            menuCandidates.addAll(addingCourses);
+        }
+
+        // 메뉴 후보를 반환한다.
+        return menuCandidates.stream()
                 .sorted()
                 .toArray(String[]::new);
     }
 
-    private void getCourses(char nextMenu, Set<String> selectedMenus,
-            List<Set<String>> orderList,
-            Map<Integer, List<Course>> courses) {
-        // 선택한 메뉴의 주문 횟수를 찾는다.
-        int occurrences = (int) orderList.stream()
-                .filter(order -> order.containsAll(selectedMenus))
-                .count();
-        
-        // 이미 2보다 적게 불렸다면 다음으로 진행하더라도 적으므로 추가로 진행하지 않는다.
-        if (occurrences < 2) {
-            return;
-        }
-
-        // course 길이에 해당하는 길이라면 변경을 시도한다.
-        int size = selectedMenus.size();
-        if (courses.containsKey(size)) {
-            List<Course> courseList = courses.get(size);
-            Course course = new Course(selectedMenus.stream()
-                    .sorted()
-                    .collect(Collectors.joining("")),
-                    occurrences);
-
-            // 원래의 주문 횟수보다 많이 불렸다면, 변경한다.
-            Course original = courseList.get(0);
-            if (original.occurrences < occurrences) {
-                courseList.clear();
-                courseList.add(course);
-            } else if (original.occurrences == occurrences) { // 중복
-                courseList.add(course);
-            }
-        }
-        
-        // 길이가 10까지만 가능하므로 추가로 진행하지 않는다.
-        if (size >= 10) {
-            return;
-        }
-
-        // A-Z 오름차순으로 중복되지 않는 문자를 넣으며 메뉴를 구성한다.
-        for (char menuChar = nextMenu; menuChar <= 'Z'; menuChar++) {
-            String menu = String.valueOf(menuChar);
-
-            selectedMenus.add(menu);
-
-            getCourses((char) (menuChar + 1), selectedMenus, orderList, courses);
-
-            selectedMenus.remove(menu);
+    private void generateCourseMenus(Set<String>[] orderSet, int size, List<Set<String>> courseMenus) {
+        for (Set<String> order : orderSet) {
+            ArrayList<String> list = new ArrayList<>(order);
+            generateCourseMenus(list, 0, new TreeSet<>(), size, courseMenus);
         }
     }
 
-    private static class Course {
-        public final String name;
-        public final int occurrences;
-
-        public Course(String name, int occurrences) {
-            this.name = name;
-            this.occurrences = occurrences;
+    private void generateCourseMenus(List<String> order, int orderIndex,
+            Set<String> courseMenu, int size, List<Set<String>> courseMenus) {
+        if (courseMenu.size() == size) {
+            courseMenus.add(new TreeSet<>(courseMenu));
+            return;
         }
+
+        for (int i = orderIndex; i < order.size(); i++) {
+            courseMenu.add(order.get(i));
+
+            generateCourseMenus(order, i + 1, courseMenu, size, courseMenus);
+
+            courseMenu.remove(order.get(i));
+        }
+    }
+
+
+    private CourseCount getCourseCount(Set<String>[] orderSet, Set<String> courseMenu) {
+        int count = (int) Arrays.stream(orderSet)
+                .filter(order -> order.containsAll(courseMenu))
+                .count();
+
+        return new CourseCount(count, String.join("", courseMenu));
+    }
+
+    private static class CourseCount {
+
+        int count;
+        String name;
+
+        public CourseCount(int count, String name) {
+            this.count = count;
+            this.name = name;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String[] orders = {"ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"};
+        int[] course = {2, 3, 4};
+
+        String[] result = solution.solution(orders, course);
+        System.out.println(Arrays.toString(result));
     }
 }
