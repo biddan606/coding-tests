b21c7ea34e97828e63c41faa41c450d9f6e4e2e6
biddan606 <153509440+biddan606@users.noreply.github.com>
2024-01-05 12:50:21 +0900

    [level 2] Title: 순위 검색, Time: 1216.98 ms, Memory: 268 MB -BaekjoonHub

--- 프로그래머스/2/72412. 순위 검색/순위 검색.java
+++ 프로그래머스/2/72412. 순위 검색/순위 검색.java
@@ -1,77 +1,81 @@
-import java.util.*;
-import java.util.function.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
 
 class Solution {
+
     public int[] solution(String[] info, String[] query) {
         // 조건별 점수들을 생성한다.
         Map<String, List<Integer>> scoresMap = buildScoresMap(info);
-        
+
         // map의 values들을 오름차순 정렬한다.
-        for (List<Integer> list : scoresMap.values()) {
-            Collections.sort(list);
-        }
+        scoresMap.values()
+                .forEach(Collections::sort);
 
         // 변환된 query들로 result를 얻는다.
-        int[] result = new int[query.length];
-        for (int i = 0; i < result.length; i++) {
-            String[] tokens = query[i].split(" (and)?");
-            String key = String.join("", Arrays.copyOf(tokens, tokens.length - 1));
-            int score = Integer.parseInt(tokens[tokens.length - 1]);
-            
-            result[i] = count(scoresMap, key, score);
-        }
-        return result;
+        return Arrays.stream(query)
+                .map(q -> {
+                    String[] tokens = q.split(" (and)?");
+                    String key = String.join("", Arrays.copyOf(tokens, tokens.length - 1));
+                    int score = Integer.parseInt(tokens[tokens.length - 1]);
+
+                    return count(scoresMap, key, score);
+                })
+                .mapToInt(Integer::intValue)
+                .toArray();
     }
-    
+
     private Map<String, List<Integer>> buildScoresMap(String[] info) {
         Map<String, List<Integer>> scoresMap = new HashMap<>();
-        
+
         for (String applicant : info) {
             String[] tokens = applicant.split(" ");
             int score = Integer.parseInt(tokens[tokens.length - 1]);
-            
-            forEachKey(0, "", tokens, key -> {
-                scoresMap.computeIfAbsent(key, k -> new ArrayList<>())
-                    .add(score);
-            });
+
+            forEachKey(0, "", tokens,
+                    key -> scoresMap.computeIfAbsent(key, k -> new ArrayList<>()).add(score));
         }
-        
+
         return scoresMap;
     }
-    
+
     private void forEachKey(int index, String prefix, String[] tokens, Consumer<String> action) {
         if (index == tokens.length - 1) {
             action.accept(prefix);
             return;
         }
-        
+
         forEachKey(index + 1, prefix + tokens[index], tokens, action);
         forEachKey(index + 1, prefix + "-", tokens, action);
     }
-    
+
     private int count(Map<String, List<Integer>> scoresMap, String key, int score) {
         if (!scoresMap.containsKey(key)) {
             return 0;
         }
         List<Integer> scores = scoresMap.get(key);
-        
+
         return scores.size() - binarySearch(scores, score);
     }
-    
+
     private int binarySearch(List<Integer> scores, int score) {
         int start = 0;
         int end = scores.size() - 1;
-        
+
         while (end > start) {
             int mid = (start + end) / 2;
-            
+
             if (scores.get(mid) >= score) {
                 end = mid;
             } else {
                 start = mid + 1;
             }
         }
-        
+
         if (scores.get(start) < score) {
             return scores.size();
         }
