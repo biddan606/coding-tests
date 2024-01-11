60f68a95dd36a05fa6d490d137a7460137301142
biddan606 <153509440+biddan606@users.noreply.github.com>
2024-01-11 16:24:42 +0900

    [level 3] Title: 등굣길, Time: 0.85 ms, Memory: 51.9 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -2,50 +2,35 @@ import java.util.*;
 
 class Solution {
     public int solution(int m, int n, int[][] puddles) {
-        int[][] pathCounts = new int[n][m];
-        for (int[] row : pathCounts) {
+        int[][] memory = new int[m + 1][n + 1];
+        for (int[] row : memory) {
             Arrays.fill(row, -1);
         }
         
-        pathCounts[0][0] = 1;
+        boolean[][] isPuddle = new boolean[m + 1][n + 1];
         for (int[] puddle : puddles) {
-            pathCounts[puddle[1] - 1][puddle[0] - 1] = 0;
+            isPuddle[puddle[0]][puddle[1]] = true;
         }
         
-        for (int row = 1; row < n; row++) {
-            if (pathCounts[row][0] == 0) {
-                    continue;
-            }
-            
-            pathCounts[row][0] = pathCounts[row - 1][0];
+        return count(1, 1, m, n, isPuddle, memory);
+    }
+    
+    private int count(int x, int y, int w, int h, boolean[][] isPuddle, int[][] memory) {
+        if (x > w || y > h) {
+            return 0;
         }
-        for (int col = 1; col < m; col++) {
-            if (pathCounts[0][col] == 0) {
-                    continue;
-            }
-            
-            pathCounts[0][col] = pathCounts[0][col - 1];
+        if (isPuddle[x][y]) {
+            return 0;
         }
         
-        for (int row = 1; row < pathCounts.length; row++) {
-            for (int col = 1; col < pathCounts[row].length; col++) {
-                if (pathCounts[row][col] == 0) {
-                    continue;
-                }
-                
-                pathCounts[row][col] = (pathCounts[row - 1][col] + pathCounts[row][col - 1]) % 1_000_000_007;
-            }
+        if (memory[x][y] != -1) {
+            return memory[x][y];
+        }
+        if (x == w && y == h) {
+            return 1;
         }
         
-        return pathCounts[pathCounts.length - 1][pathCounts[pathCounts.length - 1].length - 1];
+        memory[x][y] = (count(x + 1, y, w, h, isPuddle, memory) + count(x, y + 1, w, h, isPuddle, memory)) % 1_000_000_007;
+        return memory[x][y];
     }
 }
-/*
-1  1  1  1
-1  0 -1 -1
-1 -1 -1 -1
-
-1  1  1  1
-1  0  1  2
-1  1  2  4
-*/
