1464e9ce52d5e0fab579bdddc937e0a606856123
biddan606 <153509440+biddan606@users.noreply.github.com>
2024-01-16 01:39:00 +0900

    [level Lv.1] Title: 개인정보 수집 유효기간, Time: 2.71 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/Lv.1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/Lv.1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -8,15 +8,15 @@ class Solution {
             termMap.put(splited[0], Integer.parseInt(splited[1]));
         }
         
-        SolutionDate todayDate = getDate(today);
+        int todayDate = getDate(today);
         
         List<Integer> result = new ArrayList<>();   
         for (int i = 0; i < privacies.length; i++) {
             String[] splited = privacies[i].split(" ");
             
-            SolutionDate privacyDate = getDate(splited[0]);
+            int privacyDate = getDate(splited[0]);
             
-            SolutionDate expirationDate = plusMonth(privacyDate, termMap.get(splited[1]));
+            int expirationDate = plusMonth(privacyDate, termMap.get(splited[1]));
             
             if (isPast(expirationDate, todayDate)) {
                 result.add(i + 1);
@@ -28,48 +28,20 @@ class Solution {
             .toArray();
     }
     
-    private SolutionDate getDate(String date) {
+    private int getDate(String date) {
         String[] splited = date.split("\\.");
         int year = Integer.parseInt(splited[0]);
         int month = Integer.parseInt(splited[1]);
         int day = Integer.parseInt(splited[2]);
         
-        return new SolutionDate(year, month, day);
+        return (year * 28 * 12) + (month * 28) + day;
     }
     
-    private SolutionDate plusMonth(SolutionDate date, int month) {
-        int nextYear = date.year + (date.month + month - 1) / 12;
-        int nextMonth = (date.month + month - 1) % 12 + 1;
-            
-        return new SolutionDate(
-            nextYear,
-            nextMonth,
-            date.day
-        );
+    private int plusMonth(int date, int month) {
+        return date + (month * 28);
     }
     
-    private boolean isPast(SolutionDate target, SolutionDate source) {
-        if (target.year < source.year) {
-            return true;
-        } else if (target.year == source.year) {
-            if (target.month < source.month) {
-                return true;
-            } else if (target.month == source.month) {
-                return target.day <= source.day;
-            }
-        }
-        return false;
-    }
-    
-    private static class SolutionDate {
-        final int year;
-        final int month;
-        final int day;
-        
-        public SolutionDate(int year, int month, int day) {
-            this.year = year;
-            this.month = month;
-            this.day = day;
-        }
+    private boolean isPast(int target, int source) {
+        return target <= source;
     }
 }
