0f3d5cf2d7ccc2c11d228ba33bf1cc0f8f832ace
biddan606 <153509440+biddan606@users.noreply.github.com>
2024-02-04 15:50:24 +0900

    [level 3] Title: 길 찾기 게임, Time: 2.77 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.java
+++ 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.java
@@ -1,120 +1,97 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 class Solution {
 
     public int[][] solution(int[][] nodeinfo) {
         // 이진 트리를 만든다
-        Map<Integer, List<CustomNode>> heights = new TreeMap<>((i1, i2) -> i2 - i1);
+        CustomNode[] nodes = new CustomNode[nodeinfo.length];
         for (int i = 0; i < nodeinfo.length; i++) {
             int value = i + 1;
             int width = nodeinfo[i][0];
             int height = nodeinfo[i][1];
 
-            CustomNode newCustomNode = new CustomNode(value, width);
-
-            heights.computeIfAbsent(height, k -> new ArrayList<>())
-                    .add(newCustomNode);
+            nodes[i] = new CustomNode(value, width, height);
         }
+        
+        // 레벨 순으로 정렬을 한다.
+        Arrays.sort(nodes, (n1, n2) -> n2.height - n1.height);
 
-        CustomLinkedList linkedList = new CustomLinkedList();
-        for (List<CustomNode> nodes : heights.values()) {
-            for (CustomNode node : nodes) {
-                linkedList.add(node);
-            }
-        }
+        // 레벨 순으로 정렬된 노드들로 트리를 구성한다.
+        CustomNode root = constructTree(nodes);
 
         // 전위 순회를 기록한다.
-        List<Integer> preorderValues = linkedList.getPreorderValues();
+        List<Integer> preorderValues = new ArrayList<>();
+        preorder(root, preorderValues);
 
         // 후위 순회를 기록한다.
-        List<Integer> postorderValues = linkedList.getPostorderValues();
+        List<Integer> postorderValues = new ArrayList<>();
+        postorder(root, postorderValues);
 
         return new int[][]{
-                preorderValues.stream().mapToInt(v -> v).toArray(),
-                postorderValues.stream().mapToInt(v -> v).toArray()
+                preorderValues.stream().mapToInt(Integer::intValue).toArray(),
+                postorderValues.stream().mapToInt(Integer::intValue).toArray()
         };
     }
-
-    private static class CustomLinkedList {
-
-        CustomNode head;
-
-        public void add(CustomNode newNode) {
-            if (head == null) {
-                head = newNode;
+    
+    private CustomNode constructTree(CustomNode[] nodes) {
+        CustomNode root = nodes[0];
+        
+        for (int i = 1; i < nodes.length; i++) {
+            insert(root, nodes[i]);
+        }
+        
+        return root;
+    }
+    
+    private void insert(CustomNode root, CustomNode leaf) {
+        if (root.width > leaf.width) {
+            if (root.left == null) {
+                root.left = leaf;
                 return;
             }
-
-            CustomNode prevNode = null;
-            CustomNode node = head;
-            while (node != null) {
-                prevNode = node;
-
-                if (newNode.width < node.width) {
-                    node = node.left;
-                } else {
-                    node = node.right;
-                }
-            }
-
-            if (newNode.width < prevNode.width) {
-                prevNode.left = newNode;
-            } else {
-                prevNode.right = newNode;
-            }
-        }
-
-
-        public List<Integer> getPreorderValues() {
-            List<Integer> preorderValues = new ArrayList<>();
-
-            preorder(head, preorderValues);
-
-            return preorderValues;
-        }
-
-        private void preorder(CustomNode node, List<Integer> values) {
-            if (node == null) {
+            insert(root.left, leaf);
+        } else if (root.width < leaf.width) { 
+            if (root.right == null) {
+                root.right = leaf;
                 return;
             }
-
-            values.add(node.value);
-            preorder(node.left, values);
-            preorder(node.right, values);
+            insert(root.right, leaf);
         }
-
-        public List<Integer> getPostorderValues() {
-            List<Integer> postorderValues = new ArrayList<>();
-
-            postorder(head, postorderValues);
-
-            return postorderValues;
+    }
+    
+    private void preorder(CustomNode node, List<Integer> visited) {
+        if (node == null) {
+            return;
         }
-
-        private void postorder(CustomNode node, List<Integer> values) {
-            if (node == null) {
-                return;
-            }
-
-            postorder(node.left, values);
-            postorder(node.right, values);
-            values.add(node.value);
+        
+        visited.add(node.value);
+        preorder(node.left, visited);
+        preorder(node.right, visited);
+    }
+    
+    private void postorder(CustomNode node, List<Integer> visited) {
+        if (node == null) {
+            return;
         }
+        
+        postorder(node.left, visited);
+        postorder(node.right, visited);
+        visited.add(node.value);
     }
 
     private static class CustomNode {
 
         int value;
         int width;
+        int height;
+        
         CustomNode left;
         CustomNode right;
 
-        public CustomNode(int value, int width) {
+        public CustomNode(int value, int width, int height) {
             this.value = value;
             this.width = width;
+            this.height = height;
         }
     }
 }
