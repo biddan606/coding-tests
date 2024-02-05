be3dc2e88814bcc4be46dd84d5c1f8edbe2e0de0
biddan606 <153509440+biddan606@users.noreply.github.com>
2024-02-05 16:00:09 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 1.01 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -2,45 +2,37 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] jobs) {
-        Arrays.sort(jobs, (a, b) -> a[0] - b[0]);
+        PriorityQueue<Task> sortedByCreatedAt = 
+            new PriorityQueue<>((a, b) -> a.createdAt - b.createdAt);
+        for (int i = 0; i < jobs.length; i++) {
+            sortedByCreatedAt.add(new Task(jobs[i][0], jobs[i][1]));
+        }
+        
+        
         PriorityQueue<Task> minHeap = 
             new PriorityQueue<>((a, b) -> a.workingTime - b.workingTime);
-        int accumulatedTime = 0;
-        int currentTime = 0;
-        int jobsIndex = 0;
+        int accumulated = 0;
+        int time = 0;
         
-        while (jobsIndex < jobs.length) {
+        while (!sortedByCreatedAt.isEmpty() || !minHeap.isEmpty()) {
             // 최소힙에 진행할 수 있는 작업들을 등록한다.
-            jobsIndex = insert(minHeap, jobs, jobsIndex, currentTime);
-            // 작업을 진행한다. 현재 진행할 수 있는 작업이 없다면 시간을 1초 증가시킨다.
+            while (!sortedByCreatedAt.isEmpty() && 
+                   sortedByCreatedAt.element().createdAt <= time) {
+                Task adding = sortedByCreatedAt.remove();
+                minHeap.add(adding);
+            }
+            
             if (minHeap.isEmpty()) {
-                currentTime = jobs[jobsIndex][0];
+                time = sortedByCreatedAt.element().createdAt;
                 continue;
             }
+            
             Task task = minHeap.remove();
-            currentTime += task.workingTime;
-            accumulatedTime += currentTime - task.createdAt;
+            time += task.workingTime;
+            accumulated += time - task.createdAt;
         }
         
-        // 남은 힙 정리를 한다.
-        while (!minHeap.isEmpty()) {
-            Task task = minHeap.remove();
-            currentTime += task.workingTime;
-            accumulatedTime += currentTime - task.createdAt;
-        }
-        
-        return accumulatedTime / jobs.length;
-    }
-    
-    private int insert(PriorityQueue<Task> heap, 
-                       int[][] tasks, int startIndex, int time) {
-        int index = startIndex;
-        while (index < tasks.length && tasks[index][0] <= time) {
-            heap.add(new Task(tasks[index][0], tasks[index][1]));
-            index++;
-        }
-        
-        return index;
+        return accumulated / jobs.length;
     }
     
     private static class Task {
