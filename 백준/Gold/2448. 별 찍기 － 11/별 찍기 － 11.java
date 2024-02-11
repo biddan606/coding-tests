98226aab142719b7eefcb2ebd9594a5fb6591da7
biddan606 <153509440+biddan606@users.noreply.github.com>
2024-02-11 21:24:45 +0900

    [Gold IV] Title: 별 찍기 - 11, Time: 356 ms, Memory: 55136 KB -BaekjoonHub

--- 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.java
+++ 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.java
@@ -14,11 +14,9 @@ public class Main {
         // 맵 사이즈를 구한다.
         int triangleRowSize = 3;
         int triangleColSize = 5;
-        int colCenter = 3;
         for (int i = 3; i < n; i *= 2) {
             triangleRowSize = triangleRowSize * 2;
             triangleColSize = triangleColSize * 2 + 1;
-            colCenter *= 2;
         }
 
         // 맵을 그린다.
@@ -27,7 +25,7 @@ public class Main {
             Arrays.fill(map[row], ' ');
         }
 
-        draw(map, 0, 0, triangleRowSize, triangleColSize, colCenter);
+        draw(map, 0, 0, triangleRowSize, triangleColSize);
 
         // 결과를 출력한다.
         for (char[] row : map) {
@@ -37,7 +35,7 @@ public class Main {
         reader.close();
     }
 
-    private static void draw(char[][] map, int startRow, int startCol, int rowSize, int colSize, int colCenter) {
+    private static void draw(char[][] map, int startRow, int startCol, int rowSize, int colSize) {
         if (colSize == 5) {
             // 삼각형을 그리고 반환한다.
             map[startRow][startCol + 2] = '*';
@@ -49,13 +47,8 @@ public class Main {
 
         int nextRowSize = rowSize / 2;
         int nextColSize = colSize / 2;
-        int nextColCenter = colCenter / 2;
-        draw(map, startRow, startCol + nextColCenter, nextRowSize, nextColSize, nextColCenter);
-        draw(map, startRow + nextRowSize, startCol, nextRowSize, nextColSize, nextColCenter);
-        draw(map, startRow + nextRowSize, startCol + nextColSize + 1, nextRowSize, nextColSize, nextColCenter);
+        draw(map, startRow, startCol + nextColSize / 2 + 1, nextRowSize, nextColSize);
+        draw(map, startRow + nextRowSize, startCol, nextRowSize, nextColSize);
+        draw(map, startRow + nextRowSize, startCol + nextColSize + 1, nextRowSize, nextColSize);
     }
 }
-
-/*
-23 -> 11 -> 5
-*/
