8139a728e68d2c2c6ce5bed7d917a34fb1e5eace
biddan606 <153509440+biddan606@users.noreply.github.com>
2024-03-02 16:05:56 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.43 ms, Memory: 52.8 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,71 +1,69 @@
 import java.util.*;
 
 class Solution {    
-    private static final int POBBIDEN = -2;
+    private static final int OBSTACLE = -2;
     private static final int UNVISITED = -1;
     private static final int START = 1;
     
+    Point[] DIRECTIONS = {
+            new Point(-1, 0), // 상
+            new Point(1, 0),  // 하
+            new Point(0, -1), // 좌
+            new Point(0, 1)   // 우
+            };
+    
     public int solution(int[][] maps) {
-        // 거리를 구하는 배열을 생성한다.
+        int[][] distances = initializeDistanceArray(maps);
+        
+        Point start = new Point(0, 0);
+        distances[start.row][start.col] = START;
+        
+        bfsToFindMinumumDistance(distances, start);
+        
+        return distances[maps.length - 1][maps[0].length - 1];
+    }
+    
+    private int[][] initializeDistanceArray(int[][] maps) {
         int rowLength = maps.length;
         int colLength = maps[0].length;
         int[][] distances = new int[rowLength][colLength];
+        
         for (int row = 0; row < rowLength; row++) {
             Arrays.fill(distances[row], UNVISITED);
             for (int col = 0; col < colLength; col++) {
                 if (maps[row][col] == 0) {
-                    distances[row][col] = POBBIDEN;
+                    distances[row][col] = OBSTACLE;
                 }
             }
         }
         
-        // 최소 거리를 구한다.
-        Point start = new Point(0, 0);
-        distances[start.row][start.col] = START;
-        
-        searchMinimumDistance(distances, start);
-        
-        // 도착 지점까지의 최소 거리를 반환한다. 존재하지 않을 경우 -1을 반환한다.
-        return distances[rowLength - 1][colLength - 1];
+        return distances;
     }
     
-    private void searchMinimumDistance(int[][] distances, Point start) {
-        int rowLength = distances.length;
-        int colLength = distances[0].length;
-        
-        Deque<Point> queue = new ArrayDeque<>();
-        queue.addLast(start);
-        
-        Point[] directions = {
-            new Point(-1, 0),
-            new Point(0, 1),
-            new Point(1, 0),
-            new Point(0, -1)
-            };
+    private void bfsToFindMinumumDistance(int[][] distances, Point start) {
+        Queue<Point> queue = new LinkedList<>();
+        queue.offer(start);
         
         while (!queue.isEmpty()) {
-            Point current = queue.removeFirst();
+            Point current = queue.poll();
             int nextDistance = distances[current.row][current.col] + 1;
             
-            for (Point direct : directions) {
+            for (Point direct : DIRECTIONS) {
                 Point next = new Point(current.row + direct.row, current.col + direct.col);
-                if (isNotWithinRange(distances, next)) {
-                    continue;
-                }
-                
-                if (distances[next.row][next.col] == UNVISITED || distances[next.row][next.col] > nextDistance) {
+                if (isValidLocation(distances, next) 
+                    && distances[next.row][next.col] == UNVISITED) {
                     distances[next.row][next.col] = nextDistance;
-                    queue.addLast(next);
+                    queue.offer(next);
                 }
             }
         }
     }
     
-    private boolean isNotWithinRange(int[][] array, Point point) {
-        if (point.row < 0 || array.length <= point.row) {
-            return true;
+    private boolean isValidLocation(int[][] array, Point point) {
+        if (point.row < 0 || point.row >= array.length) {
+            return false;
         }
-        return point.col < 0 || array[point.row].length <= point.col;
+        return point.col >= 0 && point.col < array[point.row].length;
     }
     
     private static class Point {
