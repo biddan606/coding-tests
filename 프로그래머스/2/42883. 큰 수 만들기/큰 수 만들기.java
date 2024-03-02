7887685c5d383e2e7e7034c415e4d8e648c1e6b5
biddan606 <153509440+biddan606@users.noreply.github.com>
2024-03-02 16:43:20 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.13 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -2,34 +2,25 @@ import java.util.*;
 
 class Solution {
     public String solution(String number, int k) {
-        Deque<Integer> right = new ArrayDeque<>();
+        Deque<Character> deque = new ArrayDeque<>();
         
-        Arrays.stream(number.split(""))
-            .map(Integer::parseInt)
-            .forEach(right::addLast);
-        
-        Deque<Integer> left = new ArrayDeque<>();
-        
-        while (!right.isEmpty()) {
-            left.addFirst(right.removeFirst());
-            
-            while (!left.isEmpty() && !right.isEmpty() && 0 < k
-                   && left.getFirst() < right.getFirst()) {
-                left.removeFirst();
+        for (char c : number.toCharArray()) {
+            while (k > 0 && !deque.isEmpty() && deque.getLast() < c) {
                 k--;
+                deque.removeLast();
             }
+            
+            deque.addLast(c);
         }
         
-        while (!left.isEmpty() && 0 < k) {
-            left.removeFirst();
-            k--;
+        while (k-- > 0) {
+            deque.removeLast();
         }
         
         StringBuilder result = new StringBuilder();
-        while (!left.isEmpty()) {
-            result.append(left.removeLast());
+        while (!deque.isEmpty()) {
+            result.append(deque.removeFirst());
         }
-        
         return result.toString();
     }
 }
