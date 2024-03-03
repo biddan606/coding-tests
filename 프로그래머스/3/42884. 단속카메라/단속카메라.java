c0eb199994436785a95eb71184ef3ddee9295605
biddan606 <153509440+biddan606@users.noreply.github.com>
2024-03-03 09:00:58 +0900

    [level 3] Title: 단속카메라, Time: 11.12 ms, Memory: 58.1 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -6,16 +6,23 @@ class Solution {
             .map(r -> Arrays.copyOf(r, r.length))
             .toArray(int[][]::new);
         
-        Arrays.sort(sortedRoutes, (a, b) -> Integer.compare(a[1], b[1]));
+        Arrays.sort(sortedRoutes, (a, b) -> a[1] - b[1]);
             
         int count = 1;
-        int camera = sortedRoutes[0][1];
+        int lastCamera = sortedRoutes[0][1];
         
         for (int i = 1; i < sortedRoutes.length; i++) {
-            if (sortedRoutes[i][0] > camera) {
-                count++;
-                camera = sortedRoutes[i][1];
+            /*
+            다음에 오는 끝점은 이전 끝점보다 항상 크다.
+            끝점을 기준으로 정렬했기 때문에
+            그러므로 시작점이 이전에 설치한 카메라보다 큰지만 확인해보면 된다.
+            */
+            if (sortedRoutes[i][0] <= lastCamera) {
+                continue;
             }
+            
+            count++;
+            lastCamera = sortedRoutes[i][1];
         }
         
         return count;
