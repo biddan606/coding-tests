3ca0a9d4a3c4293f44c78ead15d3eca15e15b6f9
biddan606 <153509440+biddan606@users.noreply.github.com>
2024-03-04 23:50:54 +0900

    [level 1] Title: 신고 결과 받기, Time: 3.68 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -1,53 +1,41 @@
 import java.util.*;
+import java.util.stream.*;
 
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
-        Map<String, Integer> reported = new HashMap<>();
+        Map<String, Set<String>> reports = new HashMap<>();
         for (String id : id_list) {
-            reported.put(id, 0);
+            reports.put(id, new HashSet<>());
         }
         
-        Map<String, Set<String>> reportMap = new HashMap<>();
-        for (String id : id_list) {
-            reportMap.put(id, new HashSet<>());
-        }
+        Map<String, Integer> reported = new HashMap<>();
         
         // 신고당한 횟수를 누적시킨다.
-        for (String line : report) {
-            String[] splited = line.split(" ");
-            String from = splited[0];
-            String to = splited[1];
+        for (String r : report) {
+            String[] tokens = r.split(" ");
+            String reporter = tokens[0];
+            String target = tokens[1];
             
-            if (reportMap.get(from).contains(to)) {
+            Set<String> targets = reports.get(reporter);
+            if (targets.contains(target)) {
                 continue;
             }
             
-            reportMap.get(from).add(to);
-            reported.put(to, reported.get(to) + 1);
+            targets.add(target);
+            reported.put(target, reported.getOrDefault(target, 0) + 1);
         }
         
-        // 유저를 정지시킨다.
-        Set<String> stopped = new HashSet<>();
-        for (Map.Entry<String,Integer> entry : reported.entrySet()) {
-            if (entry.getValue() < k) {
-                continue;
-            }
+        // 밴할 유저들을 얻는다.
+        Set<String> banned = reported.keySet().stream()
+            .filter(key -> reported.get(key) >= k)
+            .collect(Collectors.toSet());
+        
+        // 밴한 유저가 몇명인지 센다
+        return Arrays.stream(id_list)
+            .mapToInt(id -> (int) reports.get(id).stream()
+                     .filter(banned::contains)
+                     .count())
+            .toArray();
             
-            stopped.add(entry.getKey());
-        }
-        
-        // 정지시킨 유저의 개수를 구한다.
-        int[] notificationCounts = new int[id_list.length];
-        for (int i = 0; i < id_list.length; i++) {
-            String id = id_list[i];
-            
-            for (String reportedUser : reportMap.get(id)) {
-                if (stopped.contains(reportedUser)) {
-                    notificationCounts[i]++;
-                }
-            }
-        }
-        
-        return notificationCounts;
     }
 }
