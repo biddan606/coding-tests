f95ccd3277722d9315d71772ab596d4e3c37e75f
biddan606 <153509440+biddan606@users.noreply.github.com>
2024-03-19 15:44:19 +0900

    [Silver I] Title: 하노이 탑, Time: 468 ms, Memory: 94232 KB -BaekjoonHub

--- 백준/Silver/1914. 하노이 탑/하노이 탑.java
+++ 백준/Silver/1914. 하노이 탑/하노이 탑.java
@@ -4,12 +4,10 @@ import java.io.InputStreamReader;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 public class Main {
-    private static boolean shouldRecordPath;
+
     private static final List<String> transitionPaths = new ArrayList<>();
-    private static BigInteger[] moveCountCache;
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
@@ -18,32 +16,30 @@ public class Main {
 
         reader.close();
 
-        shouldRecordPath = topSize <= 20;
-        moveCountCache = new BigInteger[topSize + 1];
-
-        BigInteger totalMoves = hanoi(topSize, 1, 2, 3);
-
+        /*
+        원판 횟수 식 유도, 2^n + 2^(n - 1) + ... + 1 -> 기하급수적인 수열의 합 공식: a(r^n - 1) / (r - 1)
+        a = 1, r = 2 이므로 2^n - 1
+         */
+        BigInteger totalMoves = BigInteger.TWO.pow(topSize).subtract(BigInteger.ONE);
         System.out.println(totalMoves);
 
-        transitionPaths.forEach(System.out::println);
+        if (topSize <= 20) {
+            hanoi(topSize, 1, 2, 3);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        transitionPaths.forEach(path -> sb.append(path).append('\n'));
+
+        System.out.print(sb);
     }
 
-    private static BigInteger hanoi(int n, int from, int via, int to) {
+    private static void hanoi(int n, int from, int via, int to) {
         if (n == 0) {
-            return BigInteger.ZERO;
+            return;
         }
 
-        if (Objects.isNull(moveCountCache[n]) || shouldRecordPath) {
-            BigInteger movesBefore = hanoi(n - 1, from, to, via);
-            if (shouldRecordPath) {
-                transitionPaths.add(from + " " + to);
-            }
-            BigInteger movesAfter = hanoi(n - 1, via, from, to);
-
-            BigInteger totalMoves = movesBefore.add(BigInteger.ONE).add(movesAfter);
-            moveCountCache[n] = totalMoves;
-        }
-
-        return moveCountCache[n];
+        hanoi(n - 1, from, to, via);
+        transitionPaths.add(from + " " + to);
+        hanoi(n - 1, via, from, to);
     }
 }
