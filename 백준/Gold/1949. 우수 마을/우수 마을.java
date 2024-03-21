8e01a83c86b32b33d7026d801a8dae55aee98ad0
biddan606 <153509440+biddan606@users.noreply.github.com>
2024-03-21 23:56:21 +0900

    [Gold II] Title: 우수 마을, Time: 332 ms, Memory: 26936 KB -BaekjoonHub

---
+++ 백준/Gold/1949. 우수 마을/우수 마을.java
@@ -0,0 +1,101 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+
+
+    private static final int NOT_ATTENDING = 0;
+    private static final int ATTENDING = 1;
+
+    private static final int NON_EXISTENT_ID = -1;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        // 정점의 수를 입력 받는다.
+        int nodeCount = Integer.parseInt(reader.readLine());
+
+        // 가중치를 입력 받는다.
+        int[] weights = readLineAsIntArray(reader);
+
+        // 노드를 생성한다.
+        Node[] nodes = Arrays.stream(weights)
+                .mapToObj(Node::new)
+                .toArray(Node[]::new);
+
+        // 간선을 입력 받는다.
+        for (int i = 0; i < nodeCount - 1; i++) {
+            int[] tokens = readLineAsIntArray(reader);
+            int fromId = tokens[0] - 1;
+            int toId = tokens[1] - 1;
+
+            nodes[fromId].linkedIds.add(toId);
+            nodes[toId].linkedIds.add(fromId);
+        }
+        reader.close();
+
+        // 입력이 1개일 경우
+        if (nodeCount == 1) {
+            System.out.println(nodes[0].weight);
+            System.out.println("1");
+            return;
+        }
+
+        int[][] dp = new int[nodeCount][2];
+        int startId = 0;
+        treeDp(nodes, dp, startId, NON_EXISTENT_ID);
+
+        int resultCost = Math.max(dp[0][NOT_ATTENDING], dp[0][ATTENDING]);
+
+        List<Integer> resultPaths = new ArrayList<>();
+        boolean included = dp[startId][NOT_ATTENDING] < dp[startId][ATTENDING];
+        
+        // 결과를 출력한다.
+        System.out.println(resultCost);
+    }
+
+    private static int[] readLineAsIntArray(BufferedReader reader) throws IOException {
+        return Arrays.stream(reader.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+    }
+
+    private static void treeDp(Node[] nodes, int[][] dp, int currentId, int prevId) {
+        Node node = nodes[currentId];
+        dp[currentId][NOT_ATTENDING] = 0;
+        dp[currentId][ATTENDING] = node.weight;
+
+        if (node.isLeaf(prevId)) {
+            return;
+        }
+
+        for (int nextId : node.linkedIds) {
+            if (nextId == prevId) {
+                continue;
+            }
+            treeDp(nodes, dp, nextId, currentId);
+
+            dp[currentId][NOT_ATTENDING] += Math.max(dp[nextId][NOT_ATTENDING], dp[nextId][ATTENDING]);
+            dp[currentId][ATTENDING] += dp[nextId][NOT_ATTENDING];
+        }
+    }
+
+    private static class Node {
+
+        final int weight;
+        final List<Integer> linkedIds = new ArrayList<>();
+
+        public Node(int weight) {
+            this.weight = weight;
+        }
+
+        public boolean isLeaf(int prevId) {
+            return linkedIds.size() == 1 && linkedIds.get(0) == prevId;
+        }
+    }
+}
