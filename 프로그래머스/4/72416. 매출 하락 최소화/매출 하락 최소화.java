43c7dc6e040574d29d906760e6954897e6819bbb
biddan606 <153509440+biddan606@users.noreply.github.com>
2024-03-21 17:02:48 +0900

    [level 4] Title: 매출 하락 최소화, Time: 2.51 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/4/72416. 매출 하락 최소화/매출 하락 최소화.java
+++ 프로그래머스/4/72416. 매출 하락 최소화/매출 하락 최소화.java
@@ -1,12 +1,15 @@
 import java.util.*;
 
 class Solution {
+    
+    private static final int NOT_ATTENDING = 0;
+    private static final int ATTENDING = 1;
+    
     public int solution(int[] sales, int[][] links) {
         // 노드를 생성한다.
-        Node[] nodes = new Node[sales.length];
-        for (int i = 0; i < nodes.length; i++) {
-            nodes[i] = new Node(i, sales[i]);
-        }
+        Node[] nodes = Arrays.stream(sales)
+            .mapToObj(Node::new)
+            .toArray(Node[]::new);
         
         // 노드에 자식 노드를 추가한다.
         for (int[] link : links) {
@@ -20,15 +23,15 @@ class Solution {
         int[][] costs = new int[nodes.length][2];
         treeDp(nodes, costs, 0);
         
-        return Math.min(costs[0][0], costs[0][1]);
+        return Math.min(costs[0][NOT_ATTENDING], costs[0][ATTENDING]);
     }
     
     private void treeDp(Node[] nodes, int[][] costs, int id) {
         Node node = nodes[id];
-        costs[id][0] = 0;
-        costs[id][1] = node.sale;
+        costs[id][NOT_ATTENDING] = 0;
+        costs[id][ATTENDING] = node.sale;
         
-        if (node.childIds.isEmpty()) {
+        if (node.isLeaf()) {
             return;
         }
         
@@ -42,32 +45,34 @@ class Solution {
             [0]이 참석했을 경우 -> [1]로 변경
             [1]이 참석했다면 참석 안해도 되므로 추가 요금 0
             */
-            if (costs[childId][0] < costs[childId][1]) {
-                costs[id][0] += costs[childId][0];
-                costs[id][1] += costs[childId][0];
+            if (costs[childId][NOT_ATTENDING] < costs[childId][ATTENDING]) {
+                costs[id][0] += costs[childId][NOT_ATTENDING];
+                costs[id][1] += costs[childId][NOT_ATTENDING];
                 
                 // 자식 노드 중 추가되는 요금이 가장 작은 노드를 찾음
-                extraCost = Math.min(extraCost, costs[childId][1] - costs[childId][0]);
+                extraCost = Math.min(extraCost, costs[childId][ATTENDING] - costs[childId][NOT_ATTENDING]);
             } else { // 워크숍에 참석한 자식 노드를 추가한 경우
-                costs[id][0] += costs[childId][1];
-                costs[id][1] += costs[childId][1];
+                costs[id][0] += costs[childId][ATTENDING];
+                costs[id][1] += costs[childId][ATTENDING];
                 
                 // 자식 노드가 이미 참석했으므로 추가 요금 = 0
                 extraCost = 0;
             }
         }
         
-        costs[id][0] += extraCost;        
+        costs[id][NOT_ATTENDING] += extraCost;        
     }
     
     private static class Node {
-        final int id;
         final int sale;
         final List<Integer> childIds = new ArrayList<>();
         
-        public Node(int id, int sale) {
-            this.id = id;
+        public Node(int sale) {
             this.sale = sale;
+        }
+        
+        public boolean isLeaf() {
+            return childIds.isEmpty();
         }
     }
 }
