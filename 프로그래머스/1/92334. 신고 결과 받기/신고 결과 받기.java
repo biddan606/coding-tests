62c17f4565abcc775085c59fadd4ff430a8f2381
biddan606 <153509440+biddan606@users.noreply.github.com>
2024-03-30 18:10:02 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.21 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -1,41 +1,43 @@
 import java.util.*;
-import java.util.stream.*;
 
 class Solution {
-    public int[] solution(String[] id_list, String[] report, int k) {
-        Map<String, Set<String>> reports = new HashMap<>();
-        for (String id : id_list) {
-            reports.put(id, new HashSet<>());
+    public int[] solution(String[] id_list, String[] reports, int k) {
+        // 유저 닉네임과 아이디 번호를 매핑시킨다.
+        Map<String, Integer> intIds = new HashMap<>();
+        for (int i = 0; i < id_list.length; i++) {
+            intIds.put(id_list[i], i);
         }
         
-        Map<String, Integer> reported = new HashMap<>();
+        // 신고한다. 신고는 번호를 기준으로 한다.
+        Map<Integer, Set<Integer>> reportersMap = new HashMap<>();
+        for (int i = 0; i < id_list.length; i++) {
+            reportersMap.put(i, new HashSet<>());
+        }
         
-        // 신고당한 횟수를 누적시킨다.
-        for (String r : report) {
-            String[] tokens = r.split(" ");
-            String reporter = tokens[0];
-            String target = tokens[1];
+        for (String report : reports) {
+            String[] tokens = report.split(" ");
+            String reporterNickname = tokens[0];
+            String targetNickname = tokens[1];
             
-            Set<String> targets = reports.get(reporter);
-            if (targets.contains(target)) {
+            int reporterId = intIds.get(reporterNickname);
+            int targetId = intIds.get(targetNickname);
+            
+            Set<Integer> reporters = reportersMap.get(targetId);
+            reporters.add(reporterId);
+        }
+        
+        // 결과를 도출하여 반환한다.
+        int[] result = new int[id_list.length];
+        for (Set<Integer> reporters : reportersMap.values()) {
+            if (reporters.size() < k) {
                 continue;
             }
             
-            targets.add(target);
-            reported.put(target, reported.getOrDefault(target, 0) + 1);
+            for (int reporter : reporters) {
+                result[reporter]++;
+            }
         }
         
-        // 밴할 유저들을 얻는다.
-        Set<String> banned = reported.keySet().stream()
-            .filter(key -> reported.get(key) >= k)
-            .collect(Collectors.toSet());
-        
-        // 밴한 유저가 몇명인지 센다
-        return Arrays.stream(id_list)
-            .mapToInt(id -> (int) reports.get(id).stream()
-                     .filter(banned::contains)
-                     .count())
-            .toArray();
-            
+        return result;
     }
 }
