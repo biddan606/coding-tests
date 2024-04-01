128946698eb7f108e07654a6f640ab9c61087dad
biddan606 <153509440+biddan606@users.noreply.github.com>
2024-04-02 00:18:25 +0900

    [level 2] Title: [1차] 캐시, Time: 2.86 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -1,6 +1,39 @@
+import java.util.*;
+
 class Solution {
+    
+    private final LinkedList<String> cache = new LinkedList<>();
+    
     public int solution(int cacheSize, String[] cities) {
-        int answer = 0;
-        return answer;
+        int runtime = 0;
+        
+        for (String city : cities) {
+            String cityLower = city.toLowerCase();
+            
+            if (!cache.contains(cityLower)) {
+                runtime += 5;
+            } else {
+                runtime += 1;
+            }
+            
+            update(cityLower, cacheSize);
+        }
+        
+        return runtime;
+    }
+    
+    private void update(String newCity, int cacheSize) {
+        if (cacheSize == 0) {
+            return;
+        }
+        
+        if (cache.contains(newCity)) {
+            cache.remove(newCity);
+        }
+        while (cacheSize <= cache.size()) {
+            cache.removeFirst();    
+        }
+        
+        cache.add(newCity);
     }
 }
