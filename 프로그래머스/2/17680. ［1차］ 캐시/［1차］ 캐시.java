adc4a0e94855c0c08acb648958ba2912d18571d0
biddan606 <153509440+biddan606@users.noreply.github.com>
2024-04-01 23:58:33 +0900

    [level 2] Title: [1차] 캐시, Time: 6.20 ms, Memory: 94.8 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -1,48 +1,6 @@
-import java.util.*;
-
 class Solution {
-    
-    private final SortedMap<Integer, String> cacheMap = new TreeMap<>();
-    private final Set<String> cacheSet = new HashSet<>();
-    
     public int solution(int cacheSize, String[] cities) {
-        int runtime = 0;
-        int timeStamp = 0;
-        
-        for (String city : cities) {
-            String cityLower = city.toLowerCase();
-            
-            if (!cacheSet.contains(cityLower)) {
-                runtime += 5;
-            } else {
-                runtime += 1;
-            }
-            
-            update(cityLower, timeStamp, cacheSize);
-            
-            timeStamp++;
-        }
-        
-        return runtime;
-    }
-    
-    private void update(String newCity, int timeStamp, int cacheSize) {
-        if (cacheSize == 0) {
-            return;
-        }
-        
-        if (cacheSet.contains(newCity)) {
-            cacheMap.values().remove(newCity);
-            cacheSet.remove(newCity);
-        }
-        
-        if (cacheSet.size() >= cacheSize) {
-            int firstKey = cacheMap.firstKey();
-            String oldestCity = cacheMap.remove(firstKey);
-            cacheSet.remove(oldestCity);
-        }
-        
-        cacheMap.put(timeStamp, newCity);
-        cacheSet.add(newCity);
+        int answer = 0;
+        return answer;
     }
 }
