c94acc8070ca4f8d6167907a5eccab6481a6e2da
biddan606 <153509440+biddan606@users.noreply.github.com>
2024-04-03 00:13:34 +0900

    [level 3] Title: 풍선 터트리기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.java
+++ 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.java
@@ -1,14 +1,19 @@
 import java.util.*;
 
 class Solution {
+    /*
+    딱 한번만 작은 풍선을 터트리는 행위를 할 수 있다.
+    이 조건을 만족하기 위해서는, 양쪽 중에 한쪽 방향에만 나보다 작은 수가 존재해야 한다.
+    결국 V 자 형태의 값들이 추출될 것이다.
+    n == a.length 일 때, 
+    0 -> n 까지 내림차순 값들과 n -> 0까지 오름차순 값들을 합치면 V 자 형태의 값들을 추출할 수 있다.
+    제일 작은 값은 
+    */
     public int solution(int[] a) {
         List<Integer> descendingValues = getDescending(a);
         List<Integer> ascendingValues = getAscending(a);
         
-        Set<Integer> result = new HashSet<>(descendingValues);
-        result.addAll(ascendingValues);
-        
-        return result.size();
+        return descendingValues.size() + ascendingValues.size() - 1;
     }
     
     private List<Integer> getDescending(int[] array) {
