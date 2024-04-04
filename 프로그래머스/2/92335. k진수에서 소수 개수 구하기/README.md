272b77d0c19d1817af66dba426cdfff491095ef0
biddan606 <153509440+biddan606@users.noreply.github.com>
2024-04-05 00:09:49 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 2.44 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/README.md
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] k진수에서 소수 개수 구하기 - 92335 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92335#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92335) 
 
 ### 성능 요약
 
-메모리: 71.4 MB, 시간: 0.16 ms
+메모리: 72.8 MB, 시간: 2.44 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 05일 12:05:05
+2024년 04월 05일 00:09:48
 
 ### 문제 설명
 
--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
@@ -1,17 +1,39 @@
 import java.util.*;
+import java.util.stream.*;
 
 class Solution {
     public int solution(int n, int k) {
-        String[] tokens = Long.toString(n, k).split("0+");
+        String converted = convert(n, k);
+        
+        List<Long> splited = split(converted);
         
         int primeCount = 0;
-        for (String token : tokens) {
-            if (isPrime(Long.parseLong(token))) {
+        for (long number : splited) {
+            if (isPrime(number)) {
                 primeCount++;
             }
         }
         
         return primeCount;
+    }
+    
+    private String convert(int original, int radix) {
+        int current = original;
+        StringBuilder converted = new StringBuilder();
+        
+        while (current != 0) {
+            converted.append(current % radix);
+            current /= radix;
+        }
+        
+        return converted.reverse().toString();
+    }
+    
+    private List<Long> split(String str) {
+        return Arrays.stream(str.split("0+"))
+            .filter(s -> !s.isEmpty()) // isBlank()로 대체 가능, 공백 문자열이 들어오지 않아 isEmpty() 사용함
+            .map(Long::parseLong)
+            .collect(Collectors.toList());
     }
     
     private boolean isPrime(long number) {
