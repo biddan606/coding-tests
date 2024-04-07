7a957341a01c66ac717142812438ec26f0380beb
biddan606 <153509440+biddan606@users.noreply.github.com>
2024-04-08 03:00:09 +0900

    [Silver V] Title: 집합, Time: 1228 ms, Memory: 299944 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -17,35 +17,24 @@ class Main {
             String[] tokens = reader.readLine().split(" ");
             String command = tokens[0];
 
-            switch (command) {
-                case "add": {
-                    int number = Integer.parseInt(tokens[1]);
-                    myList.add(number);
-                    break;
-                }
-                case "remove": {
-                    int number = Integer.parseInt(tokens[1]);
-                    myList.remove(number);
-                    break;
-                }
-                case "check": {
-                    int number = Integer.parseInt(tokens[1]);
-                    myList.check(number);
-                    break;
-                }
-                case "toggle": {
-                    int number = Integer.parseInt(tokens[1]);
-                    myList.toggle(number);
-                    break;
-                }
-                case "all":
-                    myList.all();
-                    break;
-                case "empty":
-                    myList.empty();
-                    break;
-                default:
-                    throw new IllegalArgumentException("잘못된 명령어입니다.");
+            if (command.equals("add")) {
+                int number = Integer.parseInt(tokens[1]);
+                myList.add(number);
+            } else if (command.equals("remove")) {
+                int number = Integer.parseInt(tokens[1]);
+                myList.remove(number);
+            } else if (command.equals("check")) {
+                int number = Integer.parseInt(tokens[1]);
+                myList.check(number);
+            } else if (command.equals("toggle")) {
+                int number = Integer.parseInt(tokens[1]);
+                myList.toggle(number);
+            } else if (command.equals("all")) {
+                myList.all();
+            } else if (command.equals("empty")) {
+                myList.empty();
+            } else {
+                throw new IllegalArgumentException("잘못된 명령어입니다.");
             }
         }
 
