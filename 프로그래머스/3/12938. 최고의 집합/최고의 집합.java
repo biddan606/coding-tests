b758e9878343b77b6ddd39d5351e9a9eaca4cd26
biddan606 <153509440+biddan606@users.noreply.github.com>
2024-04-09 01:01:39 +0900

    [level 3] Title: 최고의 집합, Time: 0.02 ms, Memory: 52 MB -BaekjoonHub

--- 프로그래머스/3/12938. 최고의 집합/최고의 집합.java
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.java
@@ -1,6 +1,21 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int n, int s) {
-        int[] answer = {};
-        return answer;
+        if (n > s) {
+            return new int[]{-1};
+        }
+        
+        int[] result = new int[n];
+        int quotient = s / n;
+        int remainder = s % n;
+        
+        Arrays.fill(result, quotient);
+        
+        for (int i = n - remainder; i < n; i++) {
+            result[i]++;
+        }
+            
+        return result;
     }
 }
