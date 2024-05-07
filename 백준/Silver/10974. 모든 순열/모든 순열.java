7550310e8bbfd1e21708682f5ad40d9b1ad9ccc1
biddan606 <153509440+biddan606@users.noreply.github.com>
2024-05-07 20:05:49 +0900

    [Silver III] Title: 모든 순열, Time: 504 ms, Memory: 70228 KB -BaekjoonHub

--- 백준/Silver/10974. 모든 순열/모든 순열.java
+++ 백준/Silver/10974. 모든 순열/모든 순열.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.security.PrivilegedExceptionAction;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -14,14 +13,18 @@ public class Main {
         int N = Integer.parseInt(reader.readLine());
 
         // N번 1~N까지의 숫자를 순회한다.
-        dfs(new ArrayList<>(), new boolean[N + 1], N);
+        List<String> outputs = new ArrayList<>();
+        dfs(new ArrayList<>(), new boolean[N + 1], N, outputs);
+
+        String resultOutput = String.join("\n", outputs);
+        System.out.println(resultOutput);
     }
 
-    private static void dfs(List<Integer> list, boolean[] visited, int limit) {
+    private static void dfs(List<Integer> list, boolean[] visited, int limit, List<String> outputs) {
         // 배열의 길이와 limit가 같아지면, 배열을 출력하고 돌아간다.
         if (list.size() == limit) {
             // 리스트를 출력한다.
-            print(list);
+            outputs.add(createOutput(list));
             return;
         }
 
@@ -33,18 +36,16 @@ public class Main {
             visited[i] = true;
             list.add(i);
 
-            dfs(list, visited, limit);
+            dfs(list, visited, limit, outputs);
 
             list.remove(list.size() - 1);
             visited[i] = false;
         }
     }
 
-    private static void print(List<Integer> list) {
-        String output = list.stream()
+    private static String createOutput(List<Integer> list) {
+        return list.stream()
                 .map(String::valueOf)
                 .collect(Collectors.joining(" "));
-
-        System.out.println(output);
     }
 }
