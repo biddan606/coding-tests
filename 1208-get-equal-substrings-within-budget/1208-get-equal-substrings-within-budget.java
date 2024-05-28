5b6982b073249391ab723ddee72f33c07a85d904
biddan606 <153509440+biddan606@users.noreply.github.com>
2024-05-28 19:26:34 +0900

    Time: 8 ms (55.43%), Space: 42.8 MB (77.64%) - LeetHub

--- 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.java
+++ 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.java
@@ -2,23 +2,19 @@ import java.util.*;
 
 class Solution {
     public int equalSubstring(String s, String t, int maxCost) {
-        Queue<Integer> costQueue = new ArrayDeque<>();
-        int accumulatedCostsInQueue = 0;
         int maxLength = 0;
+        int accumulatedCost = 0;
+        int startIndex = 0;
 
         for (int i = 0; i < s.length(); i++) {
-            int currentCost = Math.abs(s.charAt(i) - t.charAt(i));
+            accumulatedCost += Math.abs(s.charAt(i) - t.charAt(i));
 
-            while (!costQueue.isEmpty() && accumulatedCostsInQueue + currentCost > maxCost) {
-                accumulatedCostsInQueue -= costQueue.poll();
+            while (startIndex <= i && accumulatedCost > maxCost) {
+                accumulatedCost -= Math.abs(s.charAt(startIndex) - t.charAt(startIndex));
+                startIndex++;
             }
 
-            if (accumulatedCostsInQueue + currentCost <= maxCost) {
-                accumulatedCostsInQueue += currentCost;
-                costQueue.offer(currentCost);
-            }
-
-            maxLength = Math.max(maxLength, costQueue.size());
+            maxLength = Math.max(maxLength, i - startIndex + 1);
         }
 
         return maxLength;
