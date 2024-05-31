d66f25f87805412609ec995bcbc7225e7dfbba28
biddan606 <153509440+biddan606@users.noreply.github.com>
2024-05-31 16:11:06 +0900

    Time: 1 ms (100%), Space: 45.8 MB (22.05%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.java
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -5,8 +5,10 @@ class Solution {
             xor ^= num;
         }
 
+        // 2의 보수로 and 연산 시 가작 작은 비트만 남음
         int lowestBit = xor & -xor;
         
+        // 가장 작은 비트 num1과 아닌 num2를 분리하여 얻음
         int[] result = new int[2];
         for (int num : nums) {
             if ((lowestBit & num) == 0) {
