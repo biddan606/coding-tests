1e313e28bc41553c63e71fb40a949cdfbc182d3f
biddan606 <153509440+biddan606@users.noreply.github.com>
2024-06-05 13:45:32 +0900

    Time: 6 ms (52.38%), Space: 45.1 MB (53.15%) - LeetHub

--- 2486-append-characters-to-string-to-make-subsequence/2486-append-characters-to-string-to-make-subsequence.java
+++ 2486-append-characters-to-string-to-make-subsequence/2486-append-characters-to-string-to-make-subsequence.java
@@ -3,11 +3,11 @@ class Solution {
         int notMachingIndex = 0;
 
         for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == t.charAt(notMachingIndex)) {
-                notMachingIndex++;
-            }
             if (notMachingIndex == t.length()) {
                 break;
+            }
+            if (s.charAt(i) == t.charAt(notMachingIndex)) {
+                notMachingIndex++;
             }
         }
 
