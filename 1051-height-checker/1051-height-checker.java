bfcb11341bb986d32d40e28ff0020b5633001a40
biddan606 <153509440+biddan606@users.noreply.github.com>
2024-06-11 02:37:33 +0900

    Time: 0 ms (100%), Space: 41.6 MB (12.81%) - LeetHub

--- 1051-height-checker/1051-height-checker.java
+++ 1051-height-checker/1051-height-checker.java
@@ -1,30 +1,20 @@
 class Solution {
     public int heightChecker(int[] heights) {
+        int[] lessThanOrEqual = new int[101];
+        for (int h : heights) {
+            lessThanOrEqual[h]++;
+        }
+        for (int i = 1; i < lessThanOrEqual.length; i++) {
+            lessThanOrEqual[i] += lessThanOrEqual[i - 1];
+        }
+
         int moveCount = 0;
         for (int i = 0; i < heights.length; i++) {
-            int expectedMinIndex = 0;
-            int equalValueCount = 0;
-            int currentHeight = heights[i];
-            
-            // 앞 인덱스는 자신보다 작아야 한다.
-            for (int frontIndex = 0; frontIndex < i; frontIndex++) {
-                if (heights[frontIndex] < currentHeight) {
-                    expectedMinIndex++;
-                } else if (heights[frontIndex] == currentHeight){
-                    equalValueCount++;
-                }
-            }
+            int h = heights[i];
 
-            // 뒤 인덱스는 자신보다 커야 한다.
-            for (int backIndex = i + 1; backIndex < heights.length; backIndex++) {
-                if (heights[backIndex] < currentHeight) {
-                    expectedMinIndex++;
-                } else if (heights[backIndex] == currentHeight){
-                    equalValueCount++;
-                }
-            }
+            int expectedMinIndex = h < 1 ? 0 : lessThanOrEqual[h - 1];
+            int expectedMaxIndex = lessThanOrEqual[h] - 1;
 
-            int expectedMaxIndex = expectedMinIndex + equalValueCount;
             if (expectedMinIndex > i || expectedMaxIndex < i) {
                 moveCount++;
             }
