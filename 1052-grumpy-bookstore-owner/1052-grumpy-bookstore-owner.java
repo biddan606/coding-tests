31919f0f13349463415bc65fcdd838d7ed7e8892
biddan606 <153509440+biddan606@users.noreply.github.com>
2024-06-21 16:36:44 +0900

    Time: 3 ms (91.49%), Space: 47.8 MB (12.15%) - LeetHub

--- 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.java
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.java
@@ -1,42 +1,35 @@
 class Solution {
     public int maxSatisfied(int[] customers, int[] grumpy, int minutes) {
-        // 가장 많은 심술 고객이 방문하는 구간을 구합니다.
-        // 해당 구간은 무적시간이라고 부릅니다.
-        int initialGrumpyCustomers = 0;
+        int size = customers.length;
+        int customerCount = 0;
+
+        // 항상 가능한 고객들을 센다.
+        for (int t = 0; t < size; t++) {
+            if (grumpy[t] == 0) {
+                customerCount += customers[t];
+            }
+        }
+
+        // 초기 영역을 구한다.
         for (int t = 0; t < minutes; t++) {
             if (grumpy[t] == 1) {
-                initialGrumpyCustomers += customers[t];
+                customerCount += customers[t];
             }
         }
 
-        int currentGrumpyCustomers = initialGrumpyCustomers;
-        int startTimeOfMaxGrumpy = 0;
-        int maxGrumpyCustomers = initialGrumpyCustomers;
-
-        for (int t = minutes; t < customers.length; t++) {
-            if (grumpy[t - minutes] == 1) {
-                currentGrumpyCustomers -= customers[t - minutes];
-            }
+        // 구간별 가능한 최대 고객 수를 구한다.
+        int maxCustomerCount = customerCount;
+        for (int t = 0; t + minutes < size; t++) {
             if (grumpy[t] == 1) {
-                currentGrumpyCustomers += customers[t];
+                customerCount -= customers[t];
+            }
+            if (grumpy[t + minutes] == 1) {
+                customerCount += customers[t + minutes];
             }
 
-            if (currentGrumpyCustomers > maxGrumpyCustomers) {
-                startTimeOfMaxGrumpy = t - minutes + 1;
-                maxGrumpyCustomers = currentGrumpyCustomers;
-            }
+            maxCustomerCount = Math.max(maxCustomerCount, customerCount);
         }
 
-        // 무적시간이거나 심술을 부리지 않는 시간에 고객을 셉니다.
-        int countedCustomers = 0;
-        for (int t = 0; t < customers.length; t++) {
-            if (grumpy[t] == 0 || 
-            (startTimeOfMaxGrumpy <= t && t < startTimeOfMaxGrumpy + minutes)) {
-                countedCustomers += customers[t];
-            }
-        }
-
-        // 카운팅한 고객을 반환합니다.
-        return countedCustomers;
+        return maxCustomerCount;
     }
 }
