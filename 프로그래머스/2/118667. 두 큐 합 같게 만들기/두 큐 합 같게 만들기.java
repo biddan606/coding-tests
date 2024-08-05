09ac6e891f3c9ce8beecf6ee07b118abcebe4f8b
biddan606 <153509440+biddan606@users.noreply.github.com>
2024-08-05 22:45:51 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 117.83 ms, Memory: 164 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.stream.Collectors;
 
 class Solution {
     public int solution(int[] array1, int[] array2) {
@@ -11,7 +12,7 @@ class Solution {
         Queue<Integer> queue1 = convertToQueue(array1);
         Queue<Integer> queue2 = convertToQueue(array2);
 
-        for (int i = 0; i < totalElementCount * 2; i++) {
+        for (int i = 0; i <= array1.length * 3; i++) {
             if (queue1Sum == queue2Sum) {
                 return i;
             }
@@ -32,18 +33,14 @@ class Solution {
         return -1;
     }
 
-    private long sum(int[] queue1) {
-        return Arrays.stream(queue1)
+    private long sum(int[] array) {
+        return Arrays.stream(array)
                 .sum();
     }
 
     private Queue<Integer> convertToQueue(int[] array) {
-        Queue<Integer> queue = new LinkedList<>();
-
-        for (int i = 0; i < array.length; i++) {
-            queue.add(array[i]);
-        }
-
-        return queue;
+        return Arrays.stream(array)
+                .boxed()
+                .collect(Collectors.toCollection(LinkedList::new));
     }
 }
