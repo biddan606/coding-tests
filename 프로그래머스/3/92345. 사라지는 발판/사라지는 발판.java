bc6d914efbf0818fccb6fb2671a074fc8b88bab3
biddan606 <153509440+biddan606@users.noreply.github.com>
2024-09-12 19:01:29 +0900

    [level 3] Title: 사라지는 발판, Time: 37.63 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/3/92345. 사라지는 발판/사라지는 발판.java
+++ 프로그래머스/3/92345. 사라지는 발판/사라지는 발판.java
@@ -1,106 +1,77 @@
 class Solution {
-    
-    private static final int ABLE = 1;
-    private static final int UNABLE = 0;
-    
-    private static final Point[] DIRECTS = {
-        new Point(-1, 0),
-        new Point(1, 0),
-        new Point(0, -1),
-        new Point(0, 1)
+
+    private static final int[][] DIRECTIONS = {
+            {1, 0},
+            {-1, 0},
+            {0, -1},
+            {0, 1}
     };
-    
+
     public int solution(int[][] board, int[] aloc, int[] bloc) {
-        // 게임을 실행한다.
-        GameResult result = game(board, new Point(aloc[1], aloc[0]), new Point(bloc[1], bloc[0]));
-        return result.turns;
-    }
-    
-    private GameResult game(int[][] board, Point player, Point opponent) {
         /*
-        플레이어가 패배한 경우 반환합니다.
-        - 4방향 중 1곳도 이동할 수 없다면 패배입니다.
-        - 플레이어가 서있는 곳에 발판이 사라진 경우 패배입니다.
+        a부터 게임을 진행한다. a가 게임을 이길 수 있다면, 최소 이동 횟수를 리턴한다.
+        이길 수 없다면 최대 이동 횟수를 리턴한다.
         */
-        if (canNotMove(board, player) || board[player.y][player.x] == UNABLE) {
+        return game(board, aloc, bloc).turns;
+    }
+
+    private GameResult game(int[][] board, int[] self, int[] other) {
+        if (board[self[0]][self[1]] == 0 || isImmovable(board, self)) {
             return new GameResult(false, 0);
         }
-        board[player.y][player.x] = UNABLE;
-        
-        
+        board[self[0]][self[1]] = 0;
+
         boolean win = false;
-        int maxTurnsToLose = Integer.MIN_VALUE;
-        int minTurnsToWin = Integer.MAX_VALUE;
-        
-        /*
-        1. 플레이어는 상하좌우 4방향으로 이동한다.
-            - 보드 범위 안이고, 발판이 있는 곳만 방문할 수 있습니다.
-        2. 다음 턴으로 진행합니다. 플레이어와 상대를 바꿉니다.(다음에 플레이할 유저는 상대이기 때문에)
-        3. 반환 값을 받아 갱신합니다.
-            - win == true 인 경우, 현재 플레이어가 진 경우입니다. max값을 갱신합니다.
-            - win == false 인 경우, 현재 플레이어가 이긴 경우입니다. min값을 갱신합니다.
-        */
-        for (Point direct : DIRECTS) {
-            Point next = new Point(player.x + direct.x, player.y + direct.y);
-            
-            if (!isWithinRange(board, next) || board[next.y][next.x] == UNABLE) {
+        int maxTurns = 0;
+        int minTurns = Integer.MAX_VALUE;
+
+        for (int[] d : DIRECTIONS) {
+            int x = self[0] + d[0];
+            int y = self[1] + d[1];
+
+            if (!isWithinRange(board, x, y)) {
                 continue;
             }
-            
-            GameResult currentResult = game(board, opponent, next);
-            if (currentResult.win) {
-                maxTurnsToLose = Math.max(currentResult.turns + 1, maxTurnsToLose);
+
+            GameResult progressResult = game(board, other, new int[]{x, y});
+            if (progressResult.win) {
+                maxTurns = Math.max(maxTurns, progressResult.turns + 1);
             } else {
                 win = true;
-                minTurnsToWin = Math.min(currentResult.turns + 1, minTurnsToWin);
+                minTurns = Math.min(minTurns, progressResult.turns + 1);
             }
         }
-        board[player.y][player.x] = ABLE;
-        
-        
-        /*
-        - win == true 인 경우, 현재 플레이어가 이겼으므로 min 값을 반환합니다.
-        - win == false 인 경우, 현재 플레이어가 한 번도 이긴 적 없는 경우입니다. 이 경우 max값을 반환합니다.
-        */
+
+        board[self[0]][self[1]] = 1;
         if (win) {
-            return new GameResult(true, minTurnsToWin);
+            return new GameResult(true, minTurns);
         }
-        return new GameResult(false, maxTurnsToLose);
+        return new GameResult(false, maxTurns);
     }
-    
-    private boolean canNotMove(int[][] board, Point player) {
-        for (Point direct : DIRECTS) {
-            Point next = new Point(player.x + direct.x, player.y + direct.y);
-            
-            if (isWithinRange(board, next) && board[next.y][next.x] == ABLE) {
+
+    private boolean isImmovable(int[][] board, int[] self) {
+        for (int[] d : DIRECTIONS) {
+            int x = self[0] + d[0];
+            int y = self[1] + d[1];
+
+            if (isWithinRange(board, x, y) && board[x][y] == 1) {
                 return false;
             }
         }
-        
+
         return true;
     }
-    
-    private boolean isWithinRange(int[][] board, Point point) {
-        if (point.y < 0 || board.length <= point.y) {
-            return false;
-        }
-        return 0 <= point.x && point.x < board[point.y].length;
+
+    private boolean isWithinRange(int[][] board, int x, int y) {
+        return x >= 0 && x < board.length
+                && y >= 0 && y < board[0].length;
     }
-    
-    private static class Point {
-        final int x;
-        final int y;
-        
-        public Point(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
-    
+
     private static class GameResult {
+
         final boolean win;
         final int turns;
-        
+
         public GameResult(boolean win, int turns) {
             this.win = win;
             this.turns = turns;
