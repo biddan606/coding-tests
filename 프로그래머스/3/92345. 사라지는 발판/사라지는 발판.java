af75595b62c257a5b1eea56ab6113788f9047d28
biddan606 <153509440+biddan606@users.noreply.github.com>
2024-09-12 23:10:06 +0900

    [level 3] Title: 사라지는 발판, Time: 37.63 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/3/92345. 사라지는 발판/사라지는 발판.java
+++ 프로그래머스/3/92345. 사라지는 발판/사라지는 발판.java
@@ -1,83 +1,80 @@
 class Solution {
 
-    static class Result {
-
-        boolean isWin;
-        int moveCnt;
-
-        public Result(boolean isWin, int moveCnt) {
-            this.isWin = isWin;
-            this.moveCnt = moveCnt;
-        }
-    }
-
-    int rowLength;
-    int colLength;
-
-    int[] cx = {-1, 1, 0, 0};
-    int[] cy = {0, 0, -1, 1};
+    private static final int[][] DIRECTIONS = {
+            {1, 0},
+            {-1, 0},
+            {0, -1},
+            {0, 1}
+    };
 
     public int solution(int[][] board, int[] aloc, int[] bloc) {
-        rowLength = board.length;
-        colLength = board[0].length;
-
-        return dfs(board, aloc[0], aloc[1], bloc[0], bloc[1], 0, 0).moveCnt;
+        /*
+        a부터 게임을 진행한다. a가 게임을 이길 수 있다면, 최소 이동 횟수를 리턴한다.
+        이길 수 없다면 최대 이동 횟수를 리턴한다.
+        */
+        return game(board, aloc, bloc).turns;
     }
 
-    public Result dfs(int[][] board, int ax, int ay, int bx, int by, int aDepth, int bDepth) {
+    private GameResult game(int[][] board, int[] self, int[] other) {
+        if (board[self[0]][self[1]] == 0 || isImmovable(board, self)) {
+            return new GameResult(false, 0);
+        }
+        board[self[0]][self[1]] = 0;
 
-        int winCnt = 5 * 5;
-        int loseCnt = aDepth + bDepth;
-        boolean finalWin = false;
+        boolean win = false;
+        int maxTurns = 0;
+        int minTurns = Integer.MAX_VALUE;
 
-        if (aDepth == bDepth && board[ax][ay] == 1) {
-            for (int i = 0; i < 4; i++) {
-                int newX = ax + cx[i];
-                int newY = ay + cy[i];
+        for (int[] d : DIRECTIONS) {
+            int x = self[0] + d[0];
+            int y = self[1] + d[1];
 
-                if (newX < 0 || newX >= rowLength || newY < 0 || newY >= colLength) {
-                    continue;
-                }
-                if (board[newX][newY] == 0) {
-                    continue;
-                }
-                board[ax][ay] = 0;
-                Result result = dfs(board, newX, newY, bx, by, aDepth + 1, bDepth);
-                board[ax][ay] = 1;
-
-                if (!result.isWin) {
-                    finalWin = true;
-                    winCnt = Math.min(result.moveCnt, winCnt);
-                } else {
-                    loseCnt = Math.max(result.moveCnt, loseCnt);
-                }
-            }
-        } else if (aDepth > bDepth && board[bx][by] == 1) {
-            for (int i = 0; i < 4; i++) {
-                int newX = bx + cx[i];
-                int newY = by + cy[i];
-
-                if (newX < 0 || newX >= rowLength || newY < 0 || newY >= colLength) {
-                    continue;
-                }
-                if (board[newX][newY] == 0) {
-                    continue;
-                }
-                board[bx][by] = 0;
-                Result result = dfs(board, ax, ay, newX, newY, aDepth, bDepth + 1);
-                board[bx][by] = 1;
-                if (!result.isWin) {
-                    finalWin = true;
-                    winCnt = Math.min(result.moveCnt, winCnt);
-                } else {
-                    loseCnt = Math.max(result.moveCnt, loseCnt);
-                }
+            if (!isWithinRange(board, x, y)) {
+                continue;
             }
 
+            GameResult progressResult = game(board, other, new int[]{x, y});
+            if (progressResult.win) {
+                maxTurns = Math.max(maxTurns, progressResult.turns + 1);
+            } else {
+                win = true;
+                minTurns = Math.min(minTurns, progressResult.turns + 1);
+            }
         }
 
-        return new Result(finalWin, finalWin ? winCnt : loseCnt);
+        board[self[0]][self[1]] = 1;
+        if (win) {
+            return new GameResult(true, minTurns);
+        }
+        return new GameResult(false, maxTurns);
+    }
 
+    private boolean isImmovable(int[][] board, int[] self) {
+        for (int[] d : DIRECTIONS) {
+            int x = self[0] + d[0];
+            int y = self[1] + d[1];
 
+            if (isWithinRange(board, x, y) && board[x][y] == 1) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private boolean isWithinRange(int[][] board, int x, int y) {
+        return x >= 0 && x < board.length
+                && y >= 0 && y < board[0].length;
+    }
+
+    private static class GameResult {
+
+        final boolean win;
+        final int turns;
+
+        public GameResult(boolean win, int turns) {
+            this.win = win;
+            this.turns = turns;
+        }
     }
 }
