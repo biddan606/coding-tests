87f27015de0fb2d8ff8db81b2c96159ae70e0029
biddan606 <153509440+biddan606@users.noreply.github.com>
2024-09-13 13:35:38 +0900

    Time: 35 ms (8.25%), Space: 45.7 MB (25.16%) - LeetHub

--- 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.java
+++ 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.java
@@ -1,15 +1,15 @@
 class Solution {
     public int countConsistentStrings(String allowed, String[] words) {
         // allowed -> set에 담습니다. 허용된 문자를 빠르게 탐색하기 위해 사용합니다.
-        Set<Character> allowedSet = new HashSet<>();
-        for (char c : allowed.toCharArray()) {
-            allowedSet.add(c);
+        boolean[] allowedChars = new boolean[26];
+        for (char ch : allowed.toCharArray()) {
+            allowedChars[ch - 'a'] = true;
         }
 
         // words를 돌며 문자열의 문자가 allowed에 모두 포함되어 있다면 count++ 시킵니다.
         int count = 0;
         for (String word : words) {
-            if (isConsistent(allowedSet, word)) {
+            if (isConsistent(allowedChars, word)) {
                 count++;
             }
         }
@@ -17,9 +17,8 @@ class Solution {
         return count;
     }
 
-    private boolean isConsistent(Set<Character> allowedChars, String word) {
+    private boolean isConsistent(boolean[] allowedChars, String word) {
         return word.chars()
-                .mapToObj(ch -> (char) ch)
-                .noneMatch(ch -> !allowedChars.contains(ch));
+                .allMatch(ch -> allowedChars[ch - 'a']);
     }
 }
