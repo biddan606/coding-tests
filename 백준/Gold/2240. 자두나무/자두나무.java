011d04ac18e06c6c5a5eb5cf7b14e49f85db3eac
biddan606 <153509440+biddan606@users.noreply.github.com>
2024-09-23 00:10:58 +0900

    [Gold V] Title: 자두나무, Time: 108 ms, Memory: 15280 KB -BaekjoonHub

--- 백준/Gold/2240. 자두나무/자두나무.java
+++ 백준/Gold/2240. 자두나무/자두나무.java
@@ -1,60 +1,55 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        // 입력을 받는다.
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String[] firstLine = reader.readLine().split(" ");
-        int time = Integer.parseInt(firstLine[0]);
-        int maxMove = Integer.parseInt(firstLine[1]);
+        int[] firstLine = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int seconds = firstLine[0];
+        int maxMoves = firstLine[1];
 
-        int[] treeLocation = new int[time + 1];
-        for (int i = 1; i <= time; i++) {
-            treeLocation[i] = Integer.parseInt(reader.readLine());
+        int[] plumLocations = new int[seconds];
+        for (int i = 0; i < seconds; i++) {
+            plumLocations[i] = Integer.parseInt(br.readLine());
         }
-        reader.close();
+        br.close();
 
-        /*
-        스코어의 최대값을 구해야 한다.
-        time번 돌아가야 한다.
-        time + 1은 time에서 그대로 오거나 move하여 다른 나무로 이동할 수 있다.
-        move 횟수는 정해져 있다.
-        추가: m번 움직인 1번, 2번 트리의 스코어 값이 같다면 둘 다 가지고 있어야 한다.
-         */
-        int[][][] scores = new int[time + 1][maxMove + 1][3];
-        for (int m = 0; m <= maxMove; m++) {
-            int tree = (m % 2) + 1;
-            if (tree == treeLocation[1]) {
-                scores[1][m][tree] = 1;
+        // [초][이동 횟수], [0]: 현재 위치, [1]: 획득한 자두의 개수
+        int[][][] dp = new int[seconds + 1][maxMoves + 1][2];
+        for (int m = 0; m <= maxMoves; m++) {
+            dp[0][m] = new int[]{1, 0};
+        }
+
+        for (int s = 1; s <= seconds; s++) {
+            int plumLocation = plumLocations[s - 1];
+            dp[s][0] = new int[]{1, dp[s - 1][0][1] + match(1, plumLocation)};
+
+            for (int m = 1; m <= maxMoves; m++) {
+                int currentLocation = dp[s - 1][m][0];
+                int stayed = dp[s - 1][m][1] + match(currentLocation, plumLocation);
+                int moved = dp[s - 1][m - 1][1] + 1;
+
+                if (stayed < moved) {
+                    dp[s][m] = new int[]{plumLocation, moved};
+                } else {
+                    dp[s][m] = new int[]{currentLocation, stayed};
+                }
             }
         }
 
-        for (int t = 2; t <= time; t++) {
-            scores[t][0][1] = scores[t - 1][0][1] + match(1, treeLocation[t]);
-            scores[t][0][2] = scores[t - 1][0][2] + match(2, treeLocation[t]);
-
-            for (int m = 1; m <= maxMove; m++) {
-                scores[t][m][1] = Math.max(scores[t - 1][m - 1][2], scores[t - 1][m][1]) + match(1, treeLocation[t]);
-                scores[t][m][2] = Math.max(scores[t - 1][m - 1][1], scores[t - 1][m][2]) + match(2, treeLocation[t]);
-            }
+        int maxCount = 0;
+        for (int m = 0; m <= maxMoves; m++) {
+            maxCount = Math.max(maxCount, dp[seconds][m][1]);
         }
-
-        // 최대값을 구하고 결과를 출력한다.
-        int result = 0;
-        for (int m = 0; m <= maxMove; m++) {
-            result = Math.max(result, scores[time][m][1]);
-            result = Math.max(result, scores[time][m][2]);
-        }
-
-        System.out.println(result);
+        System.out.println(maxCount);
     }
 
-    private static int match(int tree1, int tree2) {
-        if (tree1 == tree2) {
+    private static int match(int currentLocation, int plumLocation) {
+        if (currentLocation == plumLocation) {
             return 1;
         }
         return 0;
