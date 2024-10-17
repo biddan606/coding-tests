e754d49b62b5cebc906548f1026734137aa66229
biddan606 <153509440+biddan606@users.noreply.github.com>
2024-10-17 23:58:09 +0900

    [level 3] Title: 코딩 테스트 공부, Time: 16.19 ms, Memory: 51.9 MB -BaekjoonHub

--- 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.java
+++ 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.java
@@ -8,36 +8,31 @@ class Solution {
             maxAlgo = Math.max(maxAlgo, p[0]);
             maxCoding = Math.max(maxCoding, p[1]);
         }
-        
-        // [알고력][코딩력], 최대 알고력과 최대 코딩력만큼 배열을 할당하고 큰 값으로 초기화한다.
+
+        // [알고력][코딩력], 최대 알고력과 최대 코딩력만큼 배열을 할당한다.
         int[][] dp = new int[maxAlgo + 1][maxCoding + 1];
-        for (int i = 0; i <= maxAlgo; i++) {
-            for (int j = 0; j <= maxCoding; j++) {
-                dp[i][j] = Integer.MAX_VALUE;
+
+        // dp 초기값 설정, 초기값은 기본 문제들을 풀었을 때 필요한 시간을 의미한다.
+        for (int algo = alp; algo < dp.length; algo++) {
+            for (int coding = cop; coding < dp[algo].length; coding++) {
+                dp[algo][coding] = algo - alp + coding - cop;
             }
         }
 
-        // 시작 지점 설정
-        dp[alp][cop] = 0;
-
-        // 알고력과 코딩력이 목표치에 도달할 때까지 dp 갱신
-        for (int algo = alp; algo <= maxAlgo; algo++) {
-            for (int coding = cop; coding <= maxCoding; coding++) {
-                if (algo < maxAlgo) {
-                    dp[algo + 1][coding] = Math.min(dp[algo + 1][coding], dp[algo][coding] + 1);
-                }
-                if (coding < maxCoding) {
-                    dp[algo][coding + 1] = Math.min(dp[algo][coding + 1], dp[algo][coding] + 1);
-                }
-
+        for (int algo = alp; algo < dp.length; algo++) {
+            for (int coding = cop; coding < dp[algo].length; coding++) {
                 for (int[] p : problems) {
-                    if (algo >= p[0] && coding >= p[1]) {
-                        int nextAlgo = Math.min(maxAlgo, algo + p[2]);
-                        int nextCoding = Math.min(maxCoding, coding + p[3]);
-                        int cost = p[4];
-
-                        dp[nextAlgo][nextCoding] = Math.min(dp[nextAlgo][nextCoding], dp[algo][coding] + cost);
+                    int requiredAlp = p[0];
+                    int requiredCop = p[1];
+                    if (algo < requiredAlp || coding < requiredCop) {
+                        continue;
                     }
+                    
+                    int nextAlgo = Math.min(maxAlgo, algo + p[2]);
+                    int nextCoding = Math.min(maxCoding, coding + p[3]);
+                    int cost = p[4];
+                    
+                    dp[nextAlgo][nextCoding] = Math.min(dp[nextAlgo][nextCoding], dp[algo][coding] + cost);
                 }
             }
         }
