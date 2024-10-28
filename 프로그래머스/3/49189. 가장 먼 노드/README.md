754203e3a45bef12ddbfa574ad4d825325f10461
biddan606 <153509440+biddan606@users.noreply.github.com>
2024-10-28 22:26:28 +0900

    [level 3] Title: 가장 먼 노드, Time: 61.46 ms, Memory: 108 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/README.md
+++ 프로그래머스/3/49189. 가장 먼 노드/README.md
@@ -1,10 +1,10 @@
 # [level 3] 가장 먼 노드 - 49189 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49189) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49189?language=java) 
 
 ### 성능 요약
 
-메모리: 117 MB, 시간: 57.50 ms
+메모리: 108 MB, 시간: 61.46 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 11일 05:59:35
+2024년 10월 28일 22:26:26
 
 ### 문제 설명
 
--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,60 +1,61 @@
 import java.util.*;
 
 class Solution {
-    public int solution(int n, int[][] edge) {
-        // 노드를 연결한다.
-        Map<Integer, List<Edge>> graph = new HashMap<>();
-        for (int[] e : edge) {
-            int id1 = e[0];
-            int id2 = e[1];
-            
-            graph.computeIfAbsent(id1, k -> new ArrayList<>()).add(new Edge(id2));
-            graph.computeIfAbsent(id2, k -> new ArrayList<>()).add(new Edge(id1));
+    public int solution(int n, int[][] edges) {
+        // 각 노드들을 연결한다.
+        int nodeSize = n + 1;
+        Map<Integer, List<Integer>> linked = new HashMap<>();
+
+        for (int[] edge : edges) {
+            linked.computeIfAbsent(edge[0], k -> new ArrayList<>())
+                    .add(edge[1]);
+            linked.computeIfAbsent(edge[1], k -> new ArrayList<>())
+                    .add(edge[0]);
         }
-        
-        // 1번 노드로부터의 거리를 반환한다.
-        Map<Integer, Integer> distatnces = bfs(1, n, graph);
-            
-        // 가장 먼 노드를 찾는다.
-        int maxDistance = distatnces.values().stream()
-            .max(Integer::compare)
-            .orElse(0);
-        
-        return (int) distatnces.values().stream()
-            .filter(v -> v == maxDistance)
-            .count();
-    }
-    
-    private Map<Integer, Integer> bfs(int startId, int maxId, Map<Integer, List<Edge>> graph) {
-        Map<Integer, Integer> distances = new HashMap<>();
-        Queue<Integer> queue = new LinkedList<>();
-        
-        distances.put(startId, 0);
-        queue.offer(startId);
-        
+
+        // 1번 노드 -> 다른 노드들을 방문한다.
+        // 방문한 노드는 재방문하지 않는다.
+        // 방문할 때마다 depth를 증가시킨다. 아무것도 방문하지 않았을 때는 1이다.
+        Queue<Element> queue = new LinkedList<>();
+        queue.offer(new Element(1, 1));
+        int lastDepth = 0;
+        int lastDepthNodeCount = 0;
+        boolean[] visited = new boolean[nodeSize];
+
         while (!queue.isEmpty()) {
-            int currentId = queue.poll();
-            int nextDistance = distances.get(currentId) + 1;
-            
-            for (Edge edge : graph.getOrDefault(currentId, Collections.emptyList())) {
-                int nextId = edge.toId;
-                if (distances.containsKey(nextId)) {
-                    continue;
-                }
-                
-                distances.put(nextId, nextDistance);
-                queue.offer(nextId);
+            Element current = queue.poll();
+            if (visited[current.node]) {
+                continue;
+            }
+
+            visited[current.node] = true;
+            if (lastDepth == current.depth) {
+                lastDepthNodeCount++;
+            } else {
+                lastDepth = current.depth;
+                lastDepthNodeCount = 1;
+            }
+
+            for (int linkedNode : linked.getOrDefault(current.node, Collections.emptyList())) {
+                queue.offer(new Element(linkedNode, current.depth + 1));
             }
         }
-        
-        return distances;
+
+        // 마지막 depth를 반환한다.
+        return lastDepthNodeCount;
     }
-    
-    private static class Edge {
-        final int toId;
-        
-        public Edge(int toId) {
-            this.toId = toId;
+
+    private static class Element {
+        final int node;
+        final int depth;
+
+        public Element(int node, int depth) {
+            this.node = node;
+            this.depth = depth;
         }
+    }
+
+    public static void main(String[] args) {
+
     }
 }
