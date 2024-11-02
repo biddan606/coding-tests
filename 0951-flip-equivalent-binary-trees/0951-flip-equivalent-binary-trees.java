a3440e503ae51f24d662a8f5742c0efbf79cc848
biddan606 <153509440+biddan606@users.noreply.github.com>
2024-11-02 15:26:55 +0900

    Time: 1 ms (6.98%), Space: 41.2 MB (73.49%) - LeetHub

--- 0951-flip-equivalent-binary-trees/0951-flip-equivalent-binary-trees.java
+++ 0951-flip-equivalent-binary-trees/0951-flip-equivalent-binary-trees.java
@@ -17,61 +17,45 @@ class Solution {
     public boolean flipEquiv(TreeNode root1, TreeNode root2) {
         // flip이 되었을 때 같다는 건, 자식이 모두 같다는 것
         // 각 노드의 자식들로 Map을 만든 후, 동일한지 비교
-        Map<Integer, List<Integer>> nodeMap1 = generateNodeMap(root1);
-        Map<Integer, List<Integer>> nodeMap2 = generateNodeMap(root2);
+        Map<Integer, Integer> nodeMap1 = generateNodeMap(root1);
+        Map<Integer, Integer> nodeMap2 = generateNodeMap(root2);
         return compare(nodeMap1, nodeMap2);
     }
 
-    private static Map<Integer, List<Integer>> generateNodeMap(TreeNode root) {
-        Map<Integer, List<Integer>> nodeMap = new HashMap<>();
+    private static Map<Integer, Integer> generateNodeMap(TreeNode root) {
+        Map<Integer, Integer> nodeMap = new HashMap<>();
+        if (root != null) {
+            nodeMap.put(root.val, -1);
+            addValues(nodeMap, root);
+        }
 
-        addValues(nodeMap, root);
         return nodeMap;
     }
 
-    private static void addValues(Map<Integer, List<Integer>> nodeMap, TreeNode node) {
-        if (node == null) {
-            return;
-        }
-
-        List<Integer> children = new ArrayList<>();
+    private static void addValues(Map<Integer, Integer> nodeMap, TreeNode node) {
         if (node.left != null) {
-            children.add(node.left.val);
+            nodeMap.put(node.left.val, node.val);
+            addValues(nodeMap, node.left);
         }
         if (node.right != null) {
-            children.add(node.right.val);
+            nodeMap.put(node.right.val, node.val);
+            addValues(nodeMap, node.right);
         }
-        children.sort((a, b) -> a - b);
-
-        nodeMap.put(node.val, children);
-
-        addValues(nodeMap, node.left);
-        addValues(nodeMap, node.right);
     }
 
     private static boolean compare(
-            Map<Integer, List<Integer>> nodeMap1,
-            Map<Integer, List<Integer>> nodeMap2) {
+            Map<Integer, Integer> nodeMap1,
+            Map<Integer, Integer> nodeMap2) {
         if (nodeMap1.size() != nodeMap2.size()) {
             return false;
         }
 
-        for (Map.Entry<Integer, List<Integer>> entry1 : nodeMap1.entrySet()) {
-            if (!nodeMap2.containsKey(entry1.getKey())) {
-                return false;
-            }
+        for (Map.Entry<Integer, Integer> entry1 : nodeMap1.entrySet()) {
+            int key1 = entry1.getKey();
+            int value1 = entry1.getValue();
 
-            List<Integer> value1 = entry1.getValue();
-            List<Integer> value2 = nodeMap2.get(entry1.getKey());
-            if (value1.size() != value2.size()) {
+            if (!nodeMap2.containsKey(key1) || nodeMap2.get(key1) != value1) {
                 return false;
-            }
-
-            int size = value1.size();
-            for (int i = 0; i < size; i++) {
-                if (value1.get(i) != value2.get(i)) {
-                    return false;
-                }
             }
         }
 
