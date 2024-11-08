6d3246bf4a78fe0c1234a4a38155064181d2661c
biddan606 <153509440+biddan606@users.noreply.github.com>
2024-11-08 13:42:48 +0900

    [Silver III] Title: 이전 순열, Time: 164 ms, Memory: 16456 KB -BaekjoonHub

---
+++ 백준/Silver/10973. 이전 순열/이전 순열.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        // 입력
+        // 1. 순열의 개수
+        // 2. 순열
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int size = Integer.parseInt(reader.readLine());
+        int[] permutation = Arrays.stream(reader.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        reader.close();
+
+        int targetIndex = size - 2;
+        while (targetIndex >= 0 && permutation[targetIndex] <= permutation[targetIndex + 1]) {
+            targetIndex--;
+        }
+        if (targetIndex < 0) {
+            System.out.println(-1);
+            return;
+        }
+
+        int swapIndex = size - 1;
+        while (permutation[swapIndex] >= permutation[targetIndex]) {
+            swapIndex--;
+        }
+
+        // swap
+        swap(permutation, swapIndex, targetIndex);
+
+        // reverse
+        for (int i = 1; targetIndex + i < size - i; i++) {
+            swap(permutation, targetIndex + i, size - i);
+        }
+
+        // 순열 출력
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+        for (int j : permutation) {
+            writer.append(String.valueOf(j))
+                    .append(" ");
+        }
+        writer.flush();
+        writer.close();
+    }
+
+    private static void swap(int[] permutation, int swapIndex, int targetIndex) {
+        int temp = permutation[swapIndex];
+        permutation[swapIndex] = permutation[targetIndex];
+        permutation[targetIndex] = temp;
+    }
+}
