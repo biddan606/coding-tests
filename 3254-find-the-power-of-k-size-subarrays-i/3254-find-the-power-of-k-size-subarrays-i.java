4aa5a33da9678271b8d2c842c61290693ec53d1a
biddan606 <153509440+biddan606@users.noreply.github.com>
2024-11-16 13:43:06 +0900

    Time: 1 ms (100%), Space: 46.1 MB (6.12%) - LeetHub

--- 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.java
+++ 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.java
@@ -1,25 +1,38 @@
 class Solution {
     public int[] resultsArray(int[] nums, int k) {
-        int size = nums.length;
-        int[][] dp = new int[size][size];
-
-        for (int i = 0; i < size; i++) {
-            dp[i][i] = nums[i];
+        if (k == 1) {
+            return nums;
         }
 
-        for (int d = 1; d < k; d++) {
-            for (int i = 0; i + d < size; i++) {
-                dp[i][i + d] = -1;
-                if (dp[i][i + d - 1] != -1 && nums[i + d - 1] + 1 == nums[i + d]) {
-                    dp[i][i + d] = nums[i + d];
-                }
+        int size = nums.length;
+        int[] result = new int[size - k + 1];
+
+        // result[0]를 구한다
+        int nonConsecutiveCount = 0;
+        for (int i = 0; i < k - 1; i++) {
+            if (nums[i] + 1 != nums[i + 1]) {
+                nonConsecutiveCount++;
             }
         }
-
-        int[] result = new int[size - k + 1];
+        result[0] = -1;
+        if (nonConsecutiveCount == 0) {
+            result[0] = nums[0 + k - 1];
+        }
         
-        for (int i = 0; i + k - 1 < size; i++) {
-            result[i] = dp[i][i + k - 1];
+        // 전체 result를 구한다
+        // 맨 앞 pair 제거, 현재 pair 추가
+        for (int i = 1; i + k - 1 < size; i++) {
+            if (nums[i - 1] + 1 != nums[i]) {
+                nonConsecutiveCount--;
+            }
+            if (nums[i + k - 2] + 1 != nums[i + k - 1]) {
+                nonConsecutiveCount++;
+            }
+
+            result[i] = -1;
+            if (nonConsecutiveCount == 0) {
+                result[i] = nums[i + k - 1];
+            }
         }
         return result;
     }
