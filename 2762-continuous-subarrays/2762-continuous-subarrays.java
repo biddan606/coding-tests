89249e1c0f2d2043a7ab183508207235db356c0d
biddan606 <153509440+biddan606@users.noreply.github.com>
2024-12-27 17:07:34 +0900

    Time: 29 ms (57.93%), Space: 57.2 MB (80.47%) - LeetHub

--- 2762-continuous-subarrays/2762-continuous-subarrays.java
+++ 2762-continuous-subarrays/2762-continuous-subarrays.java
@@ -1,27 +1,27 @@
 class Solution {
     public long continuousSubarrays(int[] nums) {
-        Map<Integer, Integer> numberCounts = new HashMap<>();
-        TreeSet<Integer> treeSet = new TreeSet<>();
-        Queue<Integer> queue = new LinkedList<>();
+        TreeMap<Integer, Integer> numberCounts = new TreeMap<>();
+        int left = 0;
+        int right = 0;
         long subarrayCount = 0L;
         
-        for (int num : nums) {
-            while (!queue.isEmpty() 
-            && (Math.abs(treeSet.first() - num) > 2 || Math.abs(treeSet.last() - num) > 2)) {
-                subarrayCount += queue.size();
+        while (right < nums.length) {
+            numberCounts.put(nums[right], numberCounts.getOrDefault(nums[right], 0) + 1);
+
+            while (!numberCounts.isEmpty() && numberCounts.lastKey() - numberCounts.firstKey() > 2) {
+                subarrayCount += right - left;
                 
-                int removed = queue.poll();
-                numberCounts.put(removed, numberCounts.get(removed) - 1);
-                if (numberCounts.get(removed) == 0) {
-                    treeSet.remove(removed);
+                numberCounts.put(nums[left], numberCounts.get(nums[left]) - 1);
+                if (numberCounts.get(nums[left]) == 0) {
+                    numberCounts.remove(nums[left]);
                 }
+                left++;
             }
 
-            queue.offer(num);
-            treeSet.add(num);
-            numberCounts.put(num, numberCounts.getOrDefault(num, 0) + 1);
+            right++;
         }
 
-        return subarrayCount + (long) queue.size() * (queue.size() + 1) / 2;
+        long remaingSize = right - left;
+        return subarrayCount + (long) remaingSize * (remaingSize + 1) / 2;
     }
 }
