952b47b4291cf25aed0651515ef9a7e2cd258391
biddan606 <153509440+biddan606@users.noreply.github.com>
2025-02-14 17:14:29 +0900

    Time: 8 ms (31.24%), Space: 42.4 MB (70.17%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.java
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.java
@@ -1,44 +1,55 @@
 class Solution {
-    /*
-    # 문제
-    s 안에 part를 순차적으로 지워야 한다
-    -> part가 없는 문자열을 반환해야 한다
-
-    # 직관
-    - s의 길이와 part의 길이가 최대 1000이므로, O(n^2)까지 가능
-    - 문자열들이 소문자로만 이루어져 있으므로 변환 필요 없음
-    - s 문자열을 제거하면 새로운 문자열이 만들어지므로 해당 자리부터 다시 검증해야 한다
-
-    # 풀이
-    1. s 안에 part가 있는지 찾는다
-        1-1 있다면 지우고 다시 s를 검사
-        1-2 없다면 반환
-    */
+    // kmp
     public String removeOccurrences(String s, String part) {
-        String result = s;
-        boolean deleted = true;
+        int sLength = s.length();
+        int partLength = part.length();
+  
+        int[] lps = new int[partLength];
+        int length = 0;
 
-        while (deleted) {
-           deleted = false;
+        for (int i = 1; i < partLength; i++) {
+            char currentLetter = part.charAt(i);
 
-           for (int resultIndex = 0; resultIndex < result.length(); resultIndex++) {
-                int partIndex = 0;
+            while (length > 0 && part.charAt(length) != currentLetter) {
+                length = lps[length - 1];
+            }
+            if (part.charAt(length) == currentLetter) {
+                length++;
+            }
 
-                while (partIndex < part.length()
-                        && resultIndex + partIndex < result.length()
-                        && part.charAt(partIndex) == result.charAt(resultIndex + partIndex)) {
-                    partIndex++;
-                }
-
-                if (partIndex == part.length()) {
-                    result = result.substring(0, resultIndex) 
-                        + result.substring(resultIndex + partIndex);
-                    deleted = true;
-                    break;
-                }
-           }
+            lps[i] = length;
         }
 
-        return result;
+        char[] result = new char[sLength];
+        int resultIndex = 0;
+        Deque<Integer> matchedLengths = new ArrayDeque<>();
+
+        for (int i = 0; i < sLength; i++) {
+            char currentLetter = s.charAt(i);
+            result[resultIndex++] = currentLetter;
+
+            int lastMatchedLength = 0;
+            if (!matchedLengths.isEmpty()) {
+                lastMatchedLength = matchedLengths.peek();
+            }
+
+            while (lastMatchedLength > 0 && part.charAt(lastMatchedLength) != currentLetter) {
+                lastMatchedLength = lps[lastMatchedLength - 1];
+            }
+            if (part.charAt(lastMatchedLength) == currentLetter) {
+                lastMatchedLength++;
+            }
+
+            matchedLengths.push(lastMatchedLength);
+
+            if (lastMatchedLength == partLength) {
+                resultIndex -= partLength;
+                for (int c = 0; c < lastMatchedLength; c++) {
+                    matchedLengths.pop();
+                }
+            }
+        }
+
+        return new String(result, 0, resultIndex);
     }
 }
