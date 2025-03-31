60d403249aa44d8b1feb7aebaa3fedc559e6f537
biddan606 <153509440+biddan606@users.noreply.github.com>
2025-03-31 19:45:07 +0900

    Time: 0 ms (100%), Space: 44.9 MB (65.47%) - LeetHub

--- 2529-maximum-count-of-positive-integer-and-negative-integer/2529-maximum-count-of-positive-integer-and-negative-integer.java
+++ 2529-maximum-count-of-positive-integer-and-negative-integer/2529-maximum-count-of-positive-integer-and-negative-integer.java
@@ -17,6 +17,18 @@ class Solution {
     5. positives의 개수를 저장한다
     6. negatives, positives 개수를 비교하여 큰 값을 찾는다
     7. 큰 값을 반환한다
+    ---
+
+    # 다른 풀이
+    이진 탐색을 이용해서 해결할 수 있다
+    
+    first-0와 last-0를 찾을 수 있다면, 
+    first-0 앞에는 last-negative가 오게되고
+    last-0 뒤에는 first-positive가 온다
+    - last-negative 앞에는 전부 negative이므로 총 negatives를 알 수 있음
+    - first-positive 뒤에는 전부 positive이므로 총 positives를 알 수 있음
+
+    first-0와 last-0는 이진 탐색으로 빠르게 찾을 수 있다
     */
     public int maximumCount(int[] nums) {
         int index = 0;
