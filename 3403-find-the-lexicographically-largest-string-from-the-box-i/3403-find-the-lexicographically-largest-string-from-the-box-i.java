b09730ce3ed8d162b3f3e1e4b82b11851b4e9f22
biddan606 <153509440+biddan606@users.noreply.github.com>
2025-06-05 01:12:20 +0900

    Time: 1 ms (100%), Space: 43.9 MB (87.04%) - LeetHub

--- 3403-find-the-lexicographically-largest-string-from-the-box-i/3403-find-the-lexicographically-largest-string-from-the-box-i.java
+++ 3403-find-the-lexicographically-largest-string-from-the-box-i/3403-find-the-lexicographically-largest-string-from-the-box-i.java
@@ -1,35 +1,28 @@
 class Solution {
+    public String lastSubstring(String s) {
+        int i = 0, j = 1, n = s.length();
+        while (j < n) {
+            int k = 0;
+            while (j + k < n && s.charAt(i + k) == s.charAt(j + k)) {
+                k++;
+            }
+            if (j + k < n && s.charAt(i + k) <= s.charAt(j + k)) {
+                int t = i;
+                i = j;
+                j = Math.max(j + 1, t + k + 1);
+            } else {
+                j = j + k + 1;
+            }
+        }
+        return s.substring(i);
+    }
+
     public String answerString(String word, int numFriends) {
         if (numFriends == 1) {
             return word;
         }
-
-        char largestAlphabet = 'a';
-        
-        for (char ch : word.toCharArray()) {
-            if (largestAlphabet < ch) {
-                largestAlphabet = ch;
-            }
-        }
-
-        int size = word.length();
-        int longestLength = size - numFriends + 1;
-        String result = "";
-
-        for (int i = 0; i < size; i++) {
-            char currentChar = word.charAt(i);
-            if (currentChar != largestAlphabet) {
-                continue;
-            }
-
-            int endIndex = Math.min(size, i + longestLength);
-            String current = word.substring(i, endIndex);
-            
-            if (result.compareTo(current) < 0) {
-                result = current;
-            }
-        }
-
-        return result;
+        String last = lastSubstring(word);
+        int n = word.length(), m = last.length();
+        return last.substring(0, Math.min(m, n - numFriends + 1));
     }
 }
