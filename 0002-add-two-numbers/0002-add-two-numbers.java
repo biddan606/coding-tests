c24fab5d0d1ec5539ec06d788c1c1508b7c5b49e
biddan606 <153509440+biddan606@users.noreply.github.com>
2025-06-18 23:20:36 +0900

    Time: 1 ms (100%), Space: 44.7 MB (41.67%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -1,5 +1,3 @@
-import java.math.BigInteger;
-
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -27,43 +25,54 @@ listNode로 반환해도 됨
 수의 길이가 총 100자까지 될 수 있어 int, long으로 안됨
 BigInteger로 풀 수 있음
 
+
+## 다른 풀이
+
+BigInteger는 간단한 풀이지만 변환 과정이 많음
+ListNode -> ListNode를 그대로 반환할 수 있음(BigO는 동일)
+
 # 분해
+
+같은 자릿수의 digit를 더하면 두자릿수가 나옴
+
+-> 십의 자릿수는 다음으로 넘기고 일의 자릿수는 현재 결과
+
+-> ListNode.next가 더 큰 값이므로 다음 결과를 재귀형태로 받으면 됨
+
+두 수의 길이는 같지 않음
+
+-> ListNode가 Null일 수도 있다는 것을 감안해야 함
 
 # 코드 번역
 
+1. 재귀를 통해 ListNode 값을 받음
+2. 재귀 함수의 결과가 null이면 반환, 존재한다면 다음 재귀를 진행함
+-> 결과적으로 ListNode가 이어지는 형태를 띔
 
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        BigInteger bigInt1 = toBigInt(l1);
-        BigInteger bigInt2 = toBigInt(l2);
-
-        BigInteger sum = bigInt1.add(bigInt2);
-
-        return toLinkedList(sum.toString());
+        return addTwoNumbersHelper(l1, l2, 0);
     }
 
-    private static BigInteger toBigInt(ListNode node) {
-        StringBuilder sb = new StringBuilder();
-
-        while (node != null) {
-            sb.append(node.val);
-            node = node.next;
+    private static ListNode addTwoNumbersHelper(ListNode l1, ListNode l2, int carry) {
+        if (l1 == null && l2 == null) {
+            return carry == 0 ? null : new ListNode(carry);
         }
 
-        sb.reverse();
-        return new BigInteger(sb.toString());
-    }
+        int sum = carry;
 
-    private static ListNode toLinkedList(String digits) {
-        ListNode dummy = new ListNode(0);
-        ListNode current = dummy;
+        sum += l1 == null ? 0 : l1.val;
+        sum += l2 == null ? 0 : l2.val;
         
-        for (int i = digits.length() - 1; i >= 0; i--) {
-            current.next = new ListNode(digits.charAt(i) - '0');
-            
-            current = current.next;
-        }
-        return dummy.next;
+        int nextCarry = sum / 10;
+        ListNode newListNode = new ListNode(sum % 10);
+        
+        ListNode nextL1 = l1 == null ? null : l1.next;
+        ListNode nextL2 = l2 == null ? null : l2.next;
+
+        newListNode.next = addTwoNumbersHelper(nextL1, nextL2, nextCarry);
+
+        return newListNode;
     }
 }
