747b3e1692e95d9fcb2b753ccb2f4ac17ea020aa
biddan606 <153509440+biddan606@users.noreply.github.com>
2025-07-08 17:08:46 +0900

    [level 2] Title: 완전범죄, Time: 1.19 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/389480. 완전범죄/완전범죄.js
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.js
@@ -1,33 +1,33 @@
 // 2^40 = 1,099,511,627,776로 dfs 불가능
 // dp로 풀어야 함
 function solution(info, n, m) {
-    // memorization: 현재 A + memory[index][b]를 탐색한다
-    //      - memory[index][b]: 현재 index에서 b일 때 만들 수 있는 A의 최소값
-    const memory = Array.from({length: info.length}, () => Array(m).fill(-1));
-    
-    // 모든 경우의 수를 탐색한다
-    steal = (index, currentB) => {
-        if (currentB >= m) {
-            return Infinity;
-        }
-        
-        if (index === info.length) {
-            return 0;
-        }
-        
-        if (memory[index][currentB] !== -1) {
-            return memory[index][currentB];
-        }
-        
-        const [costA, costB] = info[index];
-        
-        const case1 = costA + steal(index + 1, currentB);
-        const case2 = steal(index + 1, currentB + costB);
-        
-        memory[index][currentB] = Math.min(case1, case2);
-        return memory[index][currentB];
+    const dp = Array.from({length: info.length}, () => Array(m).fill(Infinity));
+    dp[0][0] = info[0][0];
+    if (info[0][1] < m) {
+        dp[0][info[0][1]] = 0;
     }
     
-    const result = steal(0, 0);
-    return result >= n ? -1 : result;
+    for (let i = 1; i < info.length; i++) {
+        const [costA, costB] = info[i];
+        
+        for (let b = 0; b < m; b++) {
+            const prev = dp[i - 1][b];
+            if (prev === Infinity) {
+                continue;
+            }
+            
+            dp[i][b] = Math.min(dp[i][b], prev + costA);
+            
+            if (b + costB < m) {
+                dp[i][b + costB] = Math.min(dp[i][b + costB], prev);
+            }
+        }
+    }
+    
+    let result = Infinity;
+    
+    dp.at(-1)
+        .forEach((a) => result = Math.min(result, a));
+    
+    return result < n ? result : -1;
 }
