1fd4f4be69c1994d7bf867b7d77d379300f253d2
biddan606 <153509440+biddan606@users.noreply.github.com>
2025-10-10 03:33:57 +0900

    [level 2] Title: 문자열 압축, Time: 4.46 ms, Memory: 97.8 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -5,9 +5,8 @@ class Solution {
         int minCompressedLength = s.length();
         
         // 가능한 모든 압축 길이를 압축하여, 최소 길이 갱신
-        for (int compressUnit = 1; compressUnit <= s.length() / 2; compressUnit++) {
-            int compressedLength = compress(s, compressUnit);
-            
+        for (int unitSize = 1; unitSize <= s.length() / 2; unitSize++) {
+            int compressedLength = calculateCompressedLength(s, unitSize);        
             minCompressedLength = Math.min(minCompressedLength, compressedLength);
         }
         
@@ -15,50 +14,41 @@ class Solution {
         return minCompressedLength;
     }
     
-    private int compress(String str, int compressUnit) {
-        int compressedLength = 0;
+    private int calculateCompressedLength(String source, int unitSize) {
+        List<String> chunks = new ArrayList<>();
         
-        // string을 unit 길이로 나눈다.
-        List<String> splited = new ArrayList<>();
-        
-        for (int startLengthInclusive = 0; startLengthInclusive < str.length(); startLengthInclusive += compressUnit) {
-            int endLengthExclusive = Math.min(startLengthInclusive + compressUnit, str.length());
-            
-            String substr = str.substring(startLengthInclusive, endLengthExclusive);
-            splited.add(substr);
+        for (int startIndex = 0; startIndex < source.length(); startIndex += unitSize) {
+            int endIndex = Math.min(startIndex + unitSize, source.length());
+            chunks.add(source.substring(startIndex, endIndex));
         }
         
-        // 나눠진 string들을 순차적으로 통합한다.
-        // 현재 문자열과 다음 문자열이 같다면, 통합할 수 있다.
+        if (chunks.isEmpty()) {
+            return 0;
+        }
         
-        // 통합되지 못하면, 현재 string과 count를 기록한다.
-        // 통합하면, count++
-        String currentStr = "";
-        int count = 0;
+        int totalLength = 0;
+        String currentChunk = chunks.get(0);
+        int repeatCount = 1;
         
-        for (String nextStr : splited) {
-            if (currentStr.equals(nextStr)) {
-                count++;
+        for (int i = 1; i < chunks.size(); i++) {
+            if (currentChunk.equals(chunks.get(i))) {
+                repeatCount++;
             } else {
-                if (count != 0) {
-                    compressedLength += currentStr.length(); 
-                    if (count > 1) {
-                        compressedLength += String.valueOf(count).length();
-                    }
-                }
-                
-                currentStr = nextStr;
-                count = 1;
+                totalLength += getCompressedChunkLength(currentChunk, repeatCount);
+                currentChunk = chunks.get(i);
+                repeatCount = 1;
             }
         }
         
-        if (count != 0) {
-            compressedLength += currentStr.length(); 
-            if (count > 1) {
-                compressedLength += String.valueOf(count).length();
-            }
+        totalLength += getCompressedChunkLength(currentChunk, repeatCount);
+        return totalLength;
+    }
+    
+    private int getCompressedChunkLength(String chunk, int repeatCount) {
+        int length = chunk.length();
+        if (repeatCount > 1) {
+            length += String.valueOf(repeatCount).length();
         }
-        
-        return compressedLength;
+        return length;
     }
 }
