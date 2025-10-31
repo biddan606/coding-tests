be73fb1bcba112cf322adee3f80ea957571f7a3d
biddan606 <153509440+biddan606@users.noreply.github.com>
2025-10-31 16:22:19 +0900

    Time: 100 ms (53.72%), Space: 42.9 MB (41.24%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.java
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -1,43 +1,50 @@
 class Solution {
-    int[] ballons;
-    int[][] dp;
-
     public int maxCoins(int[] nums) {
-        // [3, 1, 5, 8] -> [1, 3, 1, 5, 8, 1]
-        ballons = new int[nums.length + 2];
-        
-        ballons[0] = 1;
-        ballons[ballons.length - 1] = 1;
-        for (int i = 0; i < nums.length; i++) {
-            ballons[i + 1] = nums[i];
+        // 1. 가독성을 위해 배열 길이를 변수로 추출
+        int n = nums.length;
+        int[] paddedNums = new int[n + 2];
+
+        // 2. 경계값 1 추가
+        paddedNums[0] = 1;
+        paddedNums[n + 1] = 1;
+        for (int i = 0; i < n; i++) {
+            paddedNums[i + 1] = nums[i];
         }
 
-        dp = new int[ballons.length][ballons.length];
+        // 3. DP 테이블 초기화 (0으로 자동 초기화)
+        // dp[i][j]: paddedNums의 i와 j 인덱스 사이(i, j 제외)의 풍선을 모두 터뜨렸을 때의 최대 점수
+        int[][] dp = new int[n + 2][n + 2];
 
-        return burst(1, ballons.length - 2);
+        // 원본 배열에 해당하는 범위는 (0, n+1)
+        return solve(paddedNums, dp, 0, n + 1);
     }
 
-    private int burst(int startInclusive, int endInclusive) {
-        if (startInclusive > endInclusive) {
+    /**
+     * paddedNums의 (leftBoundary, rightBoundary) 범위 내 풍선들을 터뜨려 얻는 최대 점수를 반환합니다.
+     */
+    private int solve(int[] paddedNums, int[][] dp, int leftBoundary, int rightBoundary) {
+        // 4. 더 이상 터뜨릴 풍선이 없는 경우
+        if (leftBoundary + 1 == rightBoundary) {
             return 0;
         }
-        if (dp[startInclusive][endInclusive] != 0) {
-            return dp[startInclusive][endInclusive];
+
+        // 5. 이미 계산된 값이면 즉시 반환 (Memoization)
+        if (dp[leftBoundary][rightBoundary] != 0) {
+            return dp[leftBoundary][rightBoundary];
         }
 
-        int left = startInclusive - 1;
-        int right = endInclusive + 1;
         int max = 0;
-        
-        for (int i = startInclusive; i <= endInclusive; i++) {
-            int current = burst(startInclusive, i - 1)
-                + (ballons[left] * ballons[i] * ballons[right])
-                + burst(i + 1, endInclusive);
+        // 6. (leftBoundary, rightBoundary) 사이의 풍선 k를 가장 마지막에 터뜨리는 경우를 모두 시도
+        for (int k = leftBoundary + 1; k < rightBoundary; k++) {
+            // k를 마지막에 터뜨릴 때의 점수 = 왼쪽 부분 문제 + 오른쪽 부분 문제 + k를 터뜨리는 점수
+            int currentCoins = solve(paddedNums, dp, leftBoundary, k)
+                             + solve(paddedNums, dp, k, rightBoundary)
+                             + (paddedNums[leftBoundary] * paddedNums[k] * paddedNums[rightBoundary]);
 
-            max = Math.max(max, current);
+            max = Math.max(max, currentCoins);
         }
 
-        dp[startInclusive][endInclusive] = max;
+        dp[leftBoundary][rightBoundary] = max;
         return max;
-    } 
+    }
 }
