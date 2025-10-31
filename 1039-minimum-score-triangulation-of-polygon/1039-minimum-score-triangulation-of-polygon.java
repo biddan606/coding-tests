d3ed71e419bafebbfabd081043f122b6eed21728
biddan606 <153509440+biddan606@users.noreply.github.com>
2025-10-31 22:39:01 +0900

    Time: 2 ms (98.53%), Space: 42.9 MB (14.98%) - LeetHub

--- 1039-minimum-score-triangulation-of-polygon/1039-minimum-score-triangulation-of-polygon.java
+++ 1039-minimum-score-triangulation-of-polygon/1039-minimum-score-triangulation-of-polygon.java
@@ -1,31 +1,46 @@
 class Solution {
-    private int[][] dp;
-
     public int minScoreTriangulation(int[] values) {
-        dp = new int[values.length][values.length];
-
-        return triangulate(values, 0, values.length - 1);
+        int n = values.length;
+        // 'dp' 대신 'memo'를 사용하여 메모이제이션의 의도를 명확히 함
+        int[][] memo = new int[n][n];
+        
+        // 전체 다각형(0부터 n-1까지)에 대한 문제 해결을 시작
+        return solve(values, 0, n - 1, memo);
     }
 
-    private int triangulate(int[] values, int leftBoundary, int rightBoundary) {
-        if (leftBoundary + 2 > rightBoundary) {
+    /**
+     * values 배열의 i부터 j까지의 꼭짓점으로 이루어진 다각형의
+     * 삼각분할 최소 점수를 반환합니다.
+     * @param values 꼭짓점 값 배열
+     * @param i      다각형의 시작 꼭짓점 인덱스
+     * @param j      다각형의 끝 꼭짓점 인덱스
+     * @param memo   계산 결과를 저장하기 위한 메모이제이션 테이블
+     * @return       최소 점수
+     */
+    private int solve(int[] values, int i, int j, int[][] memo) {
+        // [기저 사례] 꼭짓점이 3개 미만(선 또는 점)이면 삼각형을 만들 수 없으므로 비용은 0
+        if (i + 1 >= j) {
             return 0;
         }
-        if (dp[leftBoundary][rightBoundary] != 0) {
-            return dp[leftBoundary][rightBoundary];
+
+        // [메모이제이션] 이미 계산된 문제라면 저장된 값을 즉시 반환
+        if (memo[i][j] != 0) {
+            return memo[i][j];
         }
 
-        int min = Integer.MAX_VALUE;
+        int minScore = Integer.MAX_VALUE;
 
-        for (int mid = leftBoundary + 1; mid < rightBoundary; mid++) {
-            int current = triangulate(values, leftBoundary, mid)
-                + (values[leftBoundary] * values[mid] * values[rightBoundary])
-                + triangulate(values, mid, rightBoundary);
-
-            min = Math.min(min, current);
+        // [점화식] i와 j 사이의 모든 꼭짓점 k를 순회하며
+        // (i, k, j)를 마지막 삼각형으로 선택하는 경우를 모두 테스트
+        for (int k = i + 1; k < j; k++) {
+            int currentScore = solve(values, i, k, memo)
+                             + (values[i] * values[k] * values[j])
+                             + solve(values, k, j, memo);
+            
+            minScore = Math.min(minScore, currentScore);
         }
 
-        dp[leftBoundary][rightBoundary] = min;
-        return min;
+        // 계산된 결과를 저장하고 반환
+        return memo[i][j] = minScore;
     }
 }
