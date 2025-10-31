e00570d067c9805e84636b931e26105f8180e9ff
biddan606 <153509440+biddan606@users.noreply.github.com>
2025-10-31 22:39:13 +0900

    Time: 6 ms (27.43%), Space: 42.9 MB (14.97%) - LeetHub

--- 1039-minimum-score-triangulation-of-polygon/1039-minimum-score-triangulation-of-polygon.java
+++ 1039-minimum-score-triangulation-of-polygon/1039-minimum-score-triangulation-of-polygon.java
@@ -1,46 +1,32 @@
 class Solution {
     public int minScoreTriangulation(int[] values) {
         int n = values.length;
-        // 'dp' 대신 'memo'를 사용하여 메모이제이션의 의도를 명확히 함
-        int[][] memo = new int[n][n];
-        
-        // 전체 다각형(0부터 n-1까지)에 대한 문제 해결을 시작
-        return solve(values, 0, n - 1, memo);
-    }
+        int[][] dp = new int[n][n];
 
-    /**
-     * values 배열의 i부터 j까지의 꼭짓점으로 이루어진 다각형의
-     * 삼각분할 최소 점수를 반환합니다.
-     * @param values 꼭짓점 값 배열
-     * @param i      다각형의 시작 꼭짓점 인덱스
-     * @param j      다각형의 끝 꼭짓점 인덱스
-     * @param memo   계산 결과를 저장하기 위한 메모이제이션 테이블
-     * @return       최소 점수
-     */
-    private int solve(int[] values, int i, int j, int[][] memo) {
-        // [기저 사례] 꼭짓점이 3개 미만(선 또는 점)이면 삼각형을 만들 수 없으므로 비용은 0
-        if (i + 1 >= j) {
-            return 0;
+        // len: 다각형을 구성하는 꼭짓점의 개수 (체인의 길이)
+        // 가장 작은 다각형은 삼각형(len=3)이므로 3부터 시작
+        for (int len = 3; len <= n; len++) {
+            // i: 다각형의 시작 꼭짓점
+            for (int i = 0; i <= n - len; i++) {
+                // j: 다각형의 끝 꼭짓점
+                int j = i + len - 1;
+                
+                // 해당 범위의 최소 점수를 계산하기 위해 초깃값 설정
+                dp[i][j] = Integer.MAX_VALUE;
+
+                // k: (i, j)를 경계로 하는 마지막 삼각형(i, k, j)의 중간 꼭짓점
+                // 이 루프는 점화식을 그대로 구현한 것
+                for (int k = i + 1; k < j; k++) {
+                    int score = dp[i][k] 
+                              + (values[i] * values[k] * values[j]) 
+                              + dp[k][j];
+                              
+                    dp[i][j] = Math.min(dp[i][j], score);
+                }
+            }
         }
 
-        // [메모이제이션] 이미 계산된 문제라면 저장된 값을 즉시 반환
-        if (memo[i][j] != 0) {
-            return memo[i][j];
-        }
-
-        int minScore = Integer.MAX_VALUE;
-
-        // [점화식] i와 j 사이의 모든 꼭짓점 k를 순회하며
-        // (i, k, j)를 마지막 삼각형으로 선택하는 경우를 모두 테스트
-        for (int k = i + 1; k < j; k++) {
-            int currentScore = solve(values, i, k, memo)
-                             + (values[i] * values[k] * values[j])
-                             + solve(values, k, j, memo);
-            
-            minScore = Math.min(minScore, currentScore);
-        }
-
-        // 계산된 결과를 저장하고 반환
-        return memo[i][j] = minScore;
+        // 모든 계산이 완료되면 dp[0][n-1]에 전체 다각형의 최소 점수가 저장됨
+        return dp[0][n - 1];
     }
 }
