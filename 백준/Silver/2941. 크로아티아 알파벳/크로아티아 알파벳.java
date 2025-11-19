751a8cd2e58e0f703ecf87aa37e3751126625305
biddan606 <153509440+biddan606@users.noreply.github.com>
2025-11-19 23:16:21 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 100 ms, Memory: 14408 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -3,55 +3,26 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.Queue;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
-        String str = reader.readLine();
-        reader.close();
-
-        int stringCount = 0;
-
-        for (int i = str.length() - 1; i >= 0; i--) {
-            char current =  str.charAt(i);
-
-            if (i > 0 && current == '=') {
-                char front = str.charAt(i - 1);
-
-                if (front == 'c' || front == 's') {
-                    i--;
-                } else if (front == 'z') {
-                    i--;
-                    if (i > 0 && str.charAt(i - 1) == 'd') {
-                        i--;
-                    }
-                }
-            } else if (i > 0 && current == '-') {
-                char front = str.charAt(i - 1);
-
-                if (front == 'c' || front == 'd') {
-                    i--;
-                }
-            } else if (i > 0 && current == 'j') {
-                char front =  str.charAt(i - 1);
-
-                if (front == 'n' || front == 'l') {
-                    i--;
-                }
-            }
-
-            stringCount++;
-        }
-
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        writer.write(String.valueOf(stringCount));
+        String str = reader.readLine();
+
+        // 크로아티아 알파벳 패턴 목록
+        String[] croatianAlphabets = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+
+        // 패턴과 일치하는 문자열을 길이 1짜리 문자("*")로 치환
+        for (String pattern : croatianAlphabets) {
+            str = str.replace(pattern, "*");
+        }
+
+        writer.write(String.valueOf(str.length()));
+        
+        reader.close();
         writer.flush();
         writer.close();
     }
