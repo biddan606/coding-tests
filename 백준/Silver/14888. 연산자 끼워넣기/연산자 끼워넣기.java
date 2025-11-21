3570821e8dd7d3aaf816060def47d11b4f61d25d
biddan606 <153509440+biddan606@users.noreply.github.com>
2025-11-21 17:11:30 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 96 ms, Memory: 15640 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -1,81 +1,61 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    private static int max = Integer.MIN_VALUE;
-    private static int min = Integer.MAX_VALUE;
+    static int N;
+    static int[] numbers;
+    static int max = Integer.MIN_VALUE;
+    static int min = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int numbersSize = Integer.parseInt(reader.readLine());
+        N = Integer.parseInt(br.readLine());
+        numbers = new int[N];
 
-        int[] numbers = new int[numbersSize];
-        StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
-        for (int i = 0; i < numbersSize; i++) {
-            numbers[i] = Integer.parseInt(tokenizer.nextToken());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
         }
 
-        int[] operatorCounts = new int[4];
-        tokenizer = new StringTokenizer(reader.readLine());
-        for (int i = 0; i < operatorCounts.length; i++) {
-            operatorCounts[i] = Integer.parseInt(tokenizer.nextToken());
-        }
+        st = new StringTokenizer(br.readLine());
+        int add = Integer.parseInt(st.nextToken());
+        int sub = Integer.parseInt(st.nextToken());
+        int mul = Integer.parseInt(st.nextToken());
+        int div = Integer.parseInt(st.nextToken());
 
-        reader.close();
+        // 첫 번째 숫자를 초기값으로 설정하고, 인덱스 1부터 시작
+        dfs(1, numbers[0], add, sub, mul, div);
 
-        dfs(numbers, 1, operatorCounts, numbers[0]);
-
-        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        writer.write(max + "\n" + min);
-        writer.flush();
-        writer.close();
+        System.out.println(max);
+        System.out.println(min);
     }
 
-    private static void dfs(int[] numbers, int index, int[] operatorCounts, int accumulated) {
-        if (numbers.length == index) {
-            max = Math.max(max, accumulated);
-            min = Math.min(min, accumulated);
+    // 연산자 개수를 개별 파라미터로 분리하여 상태 관리 단순화
+    private static void dfs(int index, int sum, int add, int sub, int mul, int div) {
+        // Base Case: 모든 숫자를 다 사용했을 때
+        if (index == N) {
+            max = Math.max(max, sum);
+            min = Math.min(min, sum);
             return;
         }
 
-        int number = numbers[index];
-
-        for (int i = 0; i < operatorCounts.length; i++) {
-            if (operatorCounts[i] == 0) {
-                continue;
-            }
-
-            int nextAccumulated = 0;
-
-            switch (i) {
-                case 0:
-                    nextAccumulated = accumulated + number;
-                    break;
-                case 1:
-                    nextAccumulated = accumulated - number;
-                    break;
-                case 2:
-                    nextAccumulated = accumulated * number;
-                    break;
-                case 3:
-                    nextAccumulated = accumulated / number;
-                    break;
-                default:
-                    break;
-            }
-
-            operatorCounts[i]--;
-
-            dfs(numbers, index + 1, operatorCounts, nextAccumulated);
-
-            operatorCounts[i]++;
+        // Recursive Case: 각 연산자가 남아있을 경우에만 호출
+        // 별도의 배열 복구(backtracking) 로직 없이, 파라미터에서 -1을 하여 넘김
+        if (add > 0) {
+            dfs(index + 1, sum + numbers[index], add - 1, sub, mul, div);
+        }
+        if (sub > 0) {
+            dfs(index + 1, sum - numbers[index], add, sub - 1, mul, div);
+        }
+        if (mul > 0) {
+            dfs(index + 1, sum * numbers[index], add, sub, mul - 1, div);
+        }
+        if (div > 0) {
+            dfs(index + 1, sum / numbers[index], add, sub, mul, div - 1);
         }
     }
 }
